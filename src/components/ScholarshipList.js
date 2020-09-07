68d084ce63bf577942fd437e5acba1eb3d08f575
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-09-07 10:53:43 -0700

    basic styles
    - added some basic styles for the listing page

--- src/components/ScholarshipList.js
+++ src/components/ScholarshipList.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import './scholarshipList.css';
 
 function ScholarshipList(props){
     return(
-        <div>
+        <div class="scholarship-row">
             <ul>
                 <li>{props.name}</li>
                 <li>{props.amount}</li>
                 <li>{props.deadline}</li>
                 <li>favorte btn</li>
             </ul>
-            <p>{props.website}</p>
         </div>
     );
 }
