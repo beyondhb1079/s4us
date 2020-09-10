18b1c8492b6c13030978a03e2bab87f5ac9106c8
Josue Rios <josuerios777@gmail.com>
2020-09-10 05:12:38 +0000

    use AirBnb for linting

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
 ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root'),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
