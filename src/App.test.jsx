7809931a33b3dbd1a7b836ea7927d8da0354de2e
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-09-12 22:22:44 -0700

    Update src/App.test.jsx
    Co-authored-by: Josue Rios <josuerios777@gmail.com>

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home page by default', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Home Page/i);
   expect(linkElement).toBeInTheDocument();
