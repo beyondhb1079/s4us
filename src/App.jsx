9a9071b178e40c9a0598cf7efa810efed7b021fb
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-09-12 22:38:54 -0700

    scholarship list page, component and details page (#5)
    * list page, component and details
    
    - scholarship list page, component and details page
    - routing for the list page and details page
    
    * build error fix
    
    * yarn lint and build errors fix
    
    * yarn test error fix
    
    * Update src/App.test.jsx
    
    Co-authored-by: Josue Rios <josuerios777@gmail.com>
    
    * Update src/pages/HomePage.jsx
    
    Co-authored-by: Josue Rios <josuerios777@gmail.com>
    
    Co-authored-by: Josue Rios <josuerios777@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/HomePage';
+import ScholarshipListPage from './pages/ScholarshipListPage';
+import ScholarshipDetailsPage from './pages/ScholarshipDetailsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/scholarships/:id" component={ScholarshipDetailsPage} />
+        <Route path="/scholarships" component={ScholarshipListPage} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/ScholarshipDetailsPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import scholarships from '../testdata/scholarships';
+
+function ScholarshipDetailsPage() {
+  const { id } = useParams();
+  // will get replaced once we make actual api calls
+  const scholarship = scholarships.filter((item) => item.id === id)[0];
+
+  return (
+    scholarship
+      ? (
+        <div>
+          <h1>{scholarship.name}</h1>
+          <h2>{scholarship.amount}</h2>
+          <h3>{scholarship.deadline}</h3>
+          <h3>{scholarship.website}</h3>
+          <h3>{scholarship.school}</h3>
+          <h3>{scholarship.year}</h3>
+          <p>{scholarship.description}</p>
+        </div>
+      ) : <h1>Scholarship Not Found</h1>
+  );
+}
+
+export default ScholarshipDetailsPage;
