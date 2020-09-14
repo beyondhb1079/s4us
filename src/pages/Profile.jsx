9d35c9359c0554c106f394854dfeaf7d04ad9a71
zer0kool <zer0kool@users.noreply.github.com>
2020-09-13 20:13:39 -0700

    cleaning branch

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// made for testing with Router, can be deleted by whoever is doing the HomePage
+function UserProfile() {
+  return <h1>User_profile</h1>;
+}
+
+export default UserProfile;
