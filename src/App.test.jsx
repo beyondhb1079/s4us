9dc0d578015346446d307147581bc767c280d373
Gonzalo Lara <gonzalomac@Gonzalos-MacBook-Pro.local>
2020-09-16 02:08:41 -0700

    passed all test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-/*
-
 test('renders home page by default', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Find Scholarships Today/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-*/
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ import React, { } from 'react';
 function Home() {
   return (
     <div className="App">
+      <h1>Home Page</h1>
       <h4>Hi there,</h4>
       <h1>Find Scholarships Today</h1>
-
     </div>
   );
 }
