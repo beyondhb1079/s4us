7f902c3886e3162fc0b46398efea5dd457d7439f
zer0kool <zer0kool@users.noreply.github.com>
2020-09-21 13:54:07 -0700

    Header component

---
+++ src/components/Global/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+//CSS
+import "./style/header.css";
+
+//Logo
+const logo = process.env.PUBLIC_URL + '/assets/logo/school';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+          <nav>
+              <div className="nav-wrapper blue-grey darken-4">
+                  <ul className="left">
+                     <li>
+                      <button data-target="slide-out" className="btn-flat btn-floating sidenav-trigger"><i className="material-icons">menu</i></button>
+                      </li>
+                      <li>
+                      <a alt="HomePage" href="/" className="brand-logo">
+                          <img src={`${logo}.png`} alt="iDream" className="logo"></img>
+                          iDream</a>
+                      </li>
+                  </ul>
+                  <ul className="right">
+                      <li>
+                          <button data-target="right-sidebar-nav" className="btn-flat btn-floating sidenav-trigger">
+                              <i className="material-icons">add_alert</i></button>
+                      </li>
+                  </ul>
+              </div>
+          </nav>
+      </div>
+    );
+  }
+}
