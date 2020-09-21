0e3be0818f46b8c0131027534363c1e9621cebdc
zer0kool <zer0kool@users.noreply.github.com>
2020-09-21 16:57:45 -0700

    favs componnet

---
+++ src/components/Profile/Favorites.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+//import Loading from "../favorites/Loading";
+
+import "./styles/favorites.css"
+
+const Endpoint = 'https://next.json-generator.com/api/json/get/NkrAZ7frY';
+
+export default class favorite extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            favoriteData: [],
+            isLoading: false,
+            error: null
+        };
+    }
+
+    render() {
+        const { favoriteData, isLoading, error } = this.state;
+
+        if ( !favoriteData ){ return <p className="black-text"> No Favorites found...</p>;}
+        if ( isLoading ){var fetching = <p className="black-text"> Loading Favorites Data...</p>;}
+        if ( error ){ return <p className="red-text"> {error.message} </p>;}
+
+        return (
+            <div id="favorite">
+                <h5 className="title">Favorite Feed</h5>
+                <div className="favorite">
+                    {fetching}
+                     {favoriteData.map( (scholarship, index) =>
+                     <div className="card-panel" key={index}>
+                        <a href={scholarship.url}>
+                            <div className="post">
+                                <span className="title">{scholarship.title}</span>
+                                <p className="price">{scholarship.amount}</p>
+                            </div>
+                            <div className="social"><span className="material-icons">favorite_border</span>{scholarship.views}</div>
+                        </a>
+                     </div>)}
+                </div>
+            </div>
+        );
+    }
+
+    componentDidMount = async () =>{
+        this.setState({ isLoading: true })
+        let self = this
+        let dataParsed = [];
+        fetch(Endpoint)
+            .then(response => response.json())
+            .then(function (data){
+                try{
+                    console.log(data)
+                    data.forEach( item => {
+                        let scholarship = { title: "", url: "", author:"", image:"", views:"" };
+                        if(!item.favorite){ return }
+                        scholarship.title = item.company;
+                        scholarship.amount = item.amount;
+                        scholarship.image = item.picture;
+                        dataParsed.push(scholarship)
+                    })
+                } catch (error) {console.log(`Error inside the parsing function: ${error}`)}
+            self.setState({ favoriteData: dataParsed, isLoading: false })
+
+        }).catch( error => this.setState({ error, isLoading: false }))
+    }
+}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Cards from '../components/Profile/Cards';
+import Favorites from '../components/Profile/Favorites';
 
 // made for testing with Router, can be deleted by whoever is doing the HomePage
 function UserProfile() {
@@ -7,6 +8,7 @@ function UserProfile() {
       <div>
       <h1>User_profile</h1>
       <Cards />
+      <Favorites />
       </div>
   );
 }
