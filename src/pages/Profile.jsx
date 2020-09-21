d8cd4dfd7a1be50f3d83f4f6684482dadba13174
zer0kool <zer0kool@users.noreply.github.com>
2020-09-21 13:52:41 -0700

    Initial profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Cards from '../components/User/Profile/Cards';
 
 // made for testing with Router, can be deleted by whoever is doing the HomePage
 function UserProfile() {
-  return <h1>User_profile</h1>;
+  return (
+      <div>
+      <h1>User_profile</h1>
+      <Cards />
+      </div>
+  );
 }
 
 export default UserProfile;
