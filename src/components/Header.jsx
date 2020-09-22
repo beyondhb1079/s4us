aac3a624481984ae6d1f498f60f8b995416338c8
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-09-21 20:44:53 -0700

    moved links to navBar
    - moved links for home, about and contact page to navBar for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 import LoginDialog from './LoginDialog';
+
+const buttonStyles = {
+  marginRight: '5px',
+};
 
 function Header() {
   return (
     <div className="header">
+      <Link to="/scholarships" style={buttonStyles}>
+        <Button variant="contained" color="primary">Start Your Search</Button>
+      </Link>
+
+      <Link to="/about" style={buttonStyles}>
+        <Button variant="contained" color="primary">About</Button>
+      </Link>
+
+      <Link to="/contact">
+        <Button variant="contained" color="primary">Contact</Button>
+      </Link>
       <LoginDialog />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,9 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
-
-const buttonStyles = {
-  marginRight: '5px',
-};
 
 function Home() {
   return (
     <div className="App">
       <h1>Find Scholarships Today</h1>
-
-      <Link to="/scholarships" style={buttonStyles}>
-        <Button variant="contained" color="primary">Start Your Search</Button>
-      </Link>
-
-      <Link to="/about" style={buttonStyles}>
-        <Button variant="contained" color="primary">About</Button>
-      </Link>
-
-      <Link to="/contact">
-        <Button variant="contained" color="primary">Contact</Button>
-      </Link>
     </div>
   );
 }
