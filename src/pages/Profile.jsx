1a0a5ee263f34690f68fde92dc8ad80329c6666f
zer0kool <zer0kool@users.noreply.github.com>
2020-09-23 12:42:35 -0700

    cleaned up file

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-
-// Profile Components
-//import Cards from '../components/Profile/Cards';
 import Favorites from '../components/Profile/Favorites';
 import UserModule from '../components/Profile/UserModule';
 
-// Profile page CSS
 import "../components/Profile/styles/profile.css"
-
-// Profile page structure should be here.
 
 function UserProfile() {
   return (
