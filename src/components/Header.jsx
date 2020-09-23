57e659e2b49233c7f4b323a7c698f368da5aa060
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-09-22 19:40:21 -0700

    margin, and style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,22 @@ import Button from '@material-ui/core/Button';
 import LoginDialog from './LoginDialog';
 
 const buttonStyles = {
-  marginRight: '5px',
+  margin: '5px',
 };
 
 function Header() {
   return (
     <div className="header">
-      <Link to="/scholarships" style={buttonStyles}>
-        <Button variant="contained" color="primary">Start Your Search</Button>
+      <Link to="/scholarships">
+        <Button variant="contained" color="primary" style={buttonStyles}>Start Your Search</Button>
       </Link>
 
-      <Link to="/about" style={buttonStyles}>
-        <Button variant="contained" color="primary">About</Button>
+      <Link to="/about">
+        <Button variant="contained" color="primary" style={buttonStyles}>About</Button>
       </Link>
 
       <Link to="/contact">
-        <Button variant="contained" color="primary">Contact</Button>
+        <Button variant="contained" color="primary" style={buttonStyles}>Contact</Button>
       </Link>
       <LoginDialog />
     </div>
