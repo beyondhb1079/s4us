f71abc3b0adb2453a35a014a9c4cb968a28001b4
Josue Rios <josuerios777@gmail.com>
2020-09-23 18:02:49 -0700

    Destructure scholarship properties
    This should make it easier to use those values when we change things up later

--- src/pages/ScholarshipDetails.jsx
+++ src/pages/ScholarshipDetails.jsx
@@ -6,20 +6,22 @@ function ScholarshipDetailsPage() {
   const { id } = useParams();
   // will get replaced once we make actual api calls
   const scholarship = scholarships.filter((item) => item.id === id)[0];
+  if (!scholarship) {
+    return (<h1>Scholarship Not Found</h1>);
+  }
+  
+  const {name, amount, deadline, website, school, year, description} = scholarship;
 
   return (
-    scholarship
-      ? (
         <div>
-          <h1>{scholarship.name}</h1>
-          <h2>{scholarship.amount}</h2>
-          <h3>{scholarship.deadline}</h3>
-          <h3>{scholarship.website}</h3>
-          <h3>{scholarship.school}</h3>
-          <h3>{scholarship.year}</h3>
-          <p>{scholarship.description}</p>
+          <h1>{name}</h1>
+          <h2>{amount}</h2>
+          <h3>{deadline}</h3>
+          <h3>{website}</h3>
+          <h3>{school}</h3>
+          <h3>{year}</h3>
+          <p>{description}</p>
         </div>
-      ) : <h1>Scholarship Not Found</h1>
   );
 }
 
