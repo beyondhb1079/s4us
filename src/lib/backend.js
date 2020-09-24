5b62336fd168616f2d5ea6a23ed9ad554bb4f031
Josue Rios <josuerios777@gmail.com>
2020-09-24 04:09:43 +0000

    working fetch

--- src/lib/backend.js
+++ src/lib/backend.js
@@ -3,11 +3,7 @@ import firebase from 'firebase';
 export function loadBookmarks(onLoad) {
   const db = firebase.firestore();
   db.collection('bookmarks').get().then((querySnapshot) => {
-    let bookmarks = [];
-    querySnapshot.forEach((doc) => {
-      bookmarks.push(doc.data());
-      console.log(`${doc.id} => ${doc.data().bookmark_date}`);
-    });
-    onLoad(bookmarks)
+    const bookmarks = querySnapshot.docs.map((doc) => doc.data());
+    onLoad(bookmarks);
   });
 }
