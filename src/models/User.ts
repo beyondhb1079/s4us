bfbfa714e1f9ce0ff01a0f01098d6f3197b5ea1f
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-10-10 16:37:16 -0700

    lint fixes and enums added

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,28 +1,32 @@
+import {
+  Grade, Role, ScholarshipType, Gender,
+} from './enums';
+
 interface UserProps {
-    age: number;
-    schoolYear: number;
-    role: string;
-    gender: string;
-    scholarshipPreferences: {
-        type: string,
-        major: string,
-        amount: number, 
-        school: string,
-        grade: number,
-        location: string
-    };
-    otherSettings: {
-        notifications: boolean,
-        deadlineNotifications: boolean,
-        relatedNotifications: boolean
-    }
+  age: number;
+  schoolYear: Grade;
+  role: Role;
+  gender: Gender;
+  scholarshipPreferences: {
+    type: ScholarshipType,
+    major: string,
+    amount: number,
+    school: string,
+    schoolYear: Grade,
+    location: string
+  };
+  otherSettings: {
+    notifications: boolean,
+    deadlineNotifications: boolean,
+    relatedNotifications: boolean
+  }
 }
 
-export default class User{
-    id?:string;
-    data: UserProps
+export default class User {
+  id?:string;
+  data: UserProps;
 
-    constructor(data:UserProps){
-        this.data = { ...data };
-    }
+  constructor(data:UserProps) {
+    this.data = { ...data };
+  }
 }
---
+++ src/models/enums.ts
@@ -0,0 +1,25 @@
+export enum Grade{
+  HighschoolJunior,
+  HighschoolSenior,
+  CollegeFreshman,
+  CollegeSophmore,
+  CollegeJunior,
+  CollegeSenior
+}
+export enum Role{
+  Reader,
+  Contributor,
+  Moderator,
+  Admin
+}
+export enum Gender{
+  Male,
+  Female,
+  Other
+}
+export enum ScholarshipType{
+  Academic,
+  Sports,
+  Community,
+  Organization
+}
