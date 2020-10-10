d1fd4e9c6b661730f94e12dff91107b72044d7a8
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-10-10 15:29:06 -0700

    scholarship form page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import ScholarshipDetails from './pages/ScholarshipDetails';
 import Scholarships from './pages/Scholarships';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import ScholarshipForm from './pages/ScholarshipForm';
 
 function App() {
   firebase.initializeApp(FirebaseConfig);
@@ -18,6 +19,7 @@ function App() {
     <Router>
       <Header />
       <Switch>
+        <Route path="/scholarships/new" component={ScholarshipForm} />
         <Route path="/scholarships/:id" component={ScholarshipDetails} />
         <Route path="/scholarships" component={Scholarships} />
         <Route path="/about" component={About} />
