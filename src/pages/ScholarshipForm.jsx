d7cd5652b68b788494f7b93fc25633bf20c14fac
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-10-10 16:01:48 -0700

    lint fixes

--- src/pages/ScholarshipForm.jsx
+++ src/pages/ScholarshipForm.jsx
@@ -4,18 +4,18 @@ import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-function ScholarshipForm(){
-    return(
-        <Container>
-            <h1>Submit a Scholarship</h1>
-            <FormControl>
-                <TextField id="standard-basic" label="scholarship name *" />
-                <TextField id="standard-basic" label="Deadline *" />
-                <TextField id="standard-basic" label="amount *" />
-                <TextField id="standard-basic" label="website url *" />
-                <Button variant="contained" color="primary">Submit</Button>
-            </FormControl>
-        </Container>
-    )
+function ScholarshipForm() {
+  return (
+    <Container>
+      <h1>Submit a Scholarship</h1>
+      <FormControl>
+        <TextField id="standard-basic" label="scholarship name *" />
+        <TextField id="standard-basic" label="Deadline *" />
+        <TextField id="standard-basic" label="amount *" />
+        <TextField id="standard-basic" label="website url *" />
+        <Button variant="contained" color="primary">Submit</Button>
+      </FormControl>
+    </Container>
+  );
 }
 export default ScholarshipForm;
