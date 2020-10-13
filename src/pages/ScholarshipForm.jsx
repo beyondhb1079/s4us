e1490409c0a6ff69aa76a38cb31ec70f48607d06
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-10-13 13:57:40 -0700

    input label added to top

--- src/pages/ScholarshipForm.jsx
+++ src/pages/ScholarshipForm.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(({
+  fieldStyle: {
+    margin: '10px 0;',
+  },
+}));
 
 function ScholarshipForm() {
+  const classes = useStyles();
+
   return (
-    <Container>
+    <Container style={{ width: '40%' }}>
       <h1>Submit a Scholarship</h1>
-      <FormControl>
-        <TextField id="standard-basic" label="scholarship name *" />
-        <TextField id="standard-basic" label="Deadline *" />
-        <TextField id="standard-basic" label="amount *" />
-        <TextField id="standard-basic" label="website url *" />
+      <form>
+        <TextField className={classes.fieldStyle} id="name" label="Scholarship Name" type="text" InputLabelProps={{ shrink: true }} fullWidth />
+        <TextField className={classes.fieldStyle} id="deadline" label="Deadline" type="text" InputLabelProps={{ shrink: true }} fullWidth />
+        <TextField className={classes.fieldStyle} id="amount" label="Amount" type="text" InputLabelProps={{ shrink: true }} fullWidth />
+        <TextField className={classes.fieldStyle} id="website" label="Website URL" type="text" InputLabelProps={{ shrink: true }} fullWidth />
         <Button variant="contained" color="primary">Submit</Button>
-      </FormControl>
+      </form>
     </Container>
   );
 }
