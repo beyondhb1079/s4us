8c9434f24761a5ad4ff60c10b45e022f3208d615
Josue Rios <josuerios777@gmail.com>
2020-10-23 09:27:52 -0700

    Better styling + snackbar alert

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,76 +5,52 @@ import {
   makeStyles,
   Link as MuiLink,
   Grid,
+  Snackbar,
 } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert';
-import LoginButton from './LoginButton';
+
+import { BRAND_NAME } from '../config/constants';
+import HeaderMenu from './HeaderMenu';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  header: {
     flexGrow: 1,
     padding: theme.spacing(3),
     justifyContent: 'space-between',
-  },
-  logo: {
-    float: 'left',
-    justify: 'space-between',
-    [theme.breakpoints.down('sm')]: {
-      textAlign: 'center',
-      justifyContent: 'center',
-    },
-  },
-  menu: {
     alignItems: 'center',
-    justifyContent: 'flex-end',
     [theme.breakpoints.down('sm')]: {
-      justifyContent: 'center',
+      justifyContent: 'space-around',
     },
-  },
-  item: {
-    color: theme.palette.text.secondary,
   },
 }));
 
 function Header() {
   const match = window.location.hostname.match(/s4us-pr-(\d+).onrender.com/);
   let alert = '';
+  const [open, setOpen] = React.useState(match !== null);
   if (match !== null) {
     const link = `https://github.com/beyondhb1079/s4us/pulls/${match[1]}`;
     alert = (
-      <Alert severity="info">
-        This is a preview of <a href={link}>Pull Request #{match[1]}</a>
-      </Alert>
+      <Snackbar open={open}>
+        <Alert onClose={() => setOpen(false)} severity="info">
+          This is a preview of <a href={link}>Pull Request #{match[1]}</a>
+        </Alert>
+      </Snackbar>
     );
   }
 
   const classes = useStyles();
-  const links = {
-    Scholarships: '/scholarships',
-    About: '/about',
-    Contact: '/contact',
-  };
   return (
     <Container>
       {alert}
-      <Grid container className={classes.root} spacing={3} alignItems="center">
-        <Grid item xs={12} sm={6} className={classes.logo}>
+      <Grid container className={classes.header} spacing={3}>
+        <Grid item>
           <MuiLink component={Link} to="/" variant="h4" underline="none">
-            DREAMSCHOLARS
+            {BRAND_NAME.toUpperCase()}
           </MuiLink>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <Grid container spacing={3} className={classes.menu}>
-            {Object.entries(links).map(([title, link]) => (
-              <Grid item>
-                <MuiLink component={Link} to={link} className={classes.item}>
-                  {title}
-                </MuiLink>
-              </Grid>
-            ))}
-            <Grid item>
-              <LoginButton className={classes.item} />
-            </Grid>
-          </Grid>
+        <Grid item>
+          <HeaderMenu />
         </Grid>
       </Grid>
     </Container>
