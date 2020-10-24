c54599cb7f5db0b29c810595d414f1d163017158
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2020-10-24 00:17:46 -0700

    state and onChange added

--- src/components/TextFieldComp.jsx
+++ src/components/TextFieldComp.jsx
@@ -3,12 +3,27 @@ import { TextField } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 function TextFieldComp(props) {
-  const { id, label } = props;
-  return <TextField id={id} label={label} type="text" fullWidth />;
+  const { id, label, value } = props;
+
+  function handleFieldChange(e) {
+    props.onChange(e.target.id, e.target.value);
+  }
+  return (
+    <TextField
+      id={id}
+      label={label}
+      type="text"
+      value={value}
+      onChange={handleFieldChange}
+      fullWidth
+    />
+  );
 }
 
 TextFieldComp.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 export default TextFieldComp;
--- src/pages/ScholarshipForm.jsx
+++ src/pages/ScholarshipForm.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Button } from '@material-ui/core';
 import TextFieldComp from '../components/TextFieldComp';
 
 function ScholarshipForm() {
+  const [formFieldStates, setFormFieldStates] = useState({
+    name: '',
+    deadline: new Date(),
+    description: '',
+    amountType: '',
+    amount: 0,
+    minAmount: 0,
+    maxAmount: 0,
+    website: '',
+    scholarshipType: '',
+    active: false,
+  });
+
+  function handleFieldChange(name, value) {
+    setFormFieldStates((prev) => ({ ...prev, [name]: value }));
+  }
+
   return (
     <Container maxWidth="md">
       <form>
         <h1>Submit a Scholarship</h1>
-        <TextFieldComp id="name" label="Scholarship Name *" />
-        <TextFieldComp id="description" label="Description *" />
-        <TextFieldComp id="website" label="Website *" />
+        <TextFieldComp
+          id="name"
+          label="Scholarship Name *"
+          value={formFieldStates.name}
+          onChange={handleFieldChange}
+        />
+        <TextFieldComp
+          id="description"
+          label="Description *"
+          value={formFieldStates.description}
+          onChange={handleFieldChange}
+        />
+        <TextFieldComp
+          id="website"
+          label="Website *"
+          value={formFieldStates.website}
+          onChange={handleFieldChange}
+        />
         <Button variant="contained" color="primary">
           Submit
         </Button>
