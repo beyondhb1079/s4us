42fba68636a905033a3c3496ee08bee4fdad8e9f
Josue Rios <josuerios777@gmail.com>
2020-10-26 02:13:47 -0700

    structural improvements to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,41 +2,63 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import MuiLink from '@material-ui/core/Link';
 import Container from '@material-ui/core/Container';
 import AboutCard from '../components/AboutCard';
 import testPic from '../logo.svg';
 import { BRAND_NAME } from '../config/constants';
 
-/* eslint-disable max-len */
-const developers = [
+const team = [
   {
     name: 'Josue Rios',
     img: testPic,
-    description: `I am a DACA recipient currently working as a software engineer. I came up with this project idea after participating in the Beyond HB 1079 team and noticing how painful it is to find scholarships and compile a list for undocumented students. My hope is to make this the go-to resource for scholarships for undocumented students.`,
+    description: (
+      <>
+        I am a DACA recipient currently working as a software engineer. I came
+        up with this project idea after participating in the{' '}
+        <MuiLink href="http://beyondhb1079.org/">Beyond HB 1079</MuiLink> team
+        and noticing how painful it is to find scholarships and compile a list
+        for undocumented students. My hope is to make this the go-to resource
+        for scholarships for undocumented students.
+      </>
+    ),
   },
   {
     name: 'Edwin Lopez',
     img: testPic,
-    description: `I am a software engineer from the University of California Irvine. I enjoy learning and using my skills to make software that makes an impact in communites. Being a DACA recipient has led me to have an emotional connection with ${BRAND_NAME}.`,
+    description: (
+      <>
+        I am a software engineer from the University of California Irvine. I
+        enjoy learning and using my skills to make software that makes an impact
+        in communites. Being a DACA recipient has led me to have an emotional
+        connection with {BRAND_NAME}.
+      </>
+    ),
   },
   {
     name: 'Gonzalo Lara',
     img: testPic,
-    description:
-      'My passion for computers has lead me to pursue a career in Software Engineering. I am currently a UWB CSS student completing my final year in the Computer Science and Software Engineering program. I am 24 years old, bilingual and currently a part of the DACA program.  I intend to contribute as much as possible to building a better tomorrow.',
+    description: (
+      <>
+        My passion for computers has lead me to pursue a career in Software
+        Engineering. I am currently a UWB CSS student completing my final year
+        in the Computer Science and Software Engineering program. I am 24 years
+        old, bilingual and currently a part of the DACA program. I intend to
+        contribute as much as possible to building a better tomorrow.
+      </>
+    ),
   },
   {
     name: 'Sergio Mejia',
     img: testPic,
-    description: 'Todo: about Sergio Mejia',
+    description: <>Todo: about Sergio Mejia</>,
   },
   {
     name: 'Alejandro Loaiza',
     img: testPic,
-    description: 'Todo: about Alejandro Loaiza',
+    description: <>Todo: about Alejandro Loaiza</>,
   },
 ];
-/* eslint-enable max-len */
 
 function About() {
   return (
@@ -70,14 +92,10 @@ function About() {
       </p>
 
       <h2>Meet the Developers</h2>
-      <Grid container justify="space-evenly" spacing={3}>
-        {developers.map((dev) => (
-          <Grid item xs={10} sm={6} md={4}>
-            <AboutCard
-              img={dev.img}
-              name={dev.name}
-              description={dev.description}
-            />
+      <Grid container justify="flex-start" alignItems="stretch" spacing={3}>
+        {team.map(({ name, img, description }) => (
+          <Grid item key={name} xs={12} sm={6} md={4}>
+            <AboutCard {...{ name, img, description }} />
           </Grid>
         ))}
       </Grid>
@@ -87,11 +105,13 @@ function About() {
         Laborum aliqua labore nulla non consequat mollit nostrud quis commodo
         tempor labore irure.
       </p>
-      <Link to="/scholarships">
-        <Button variant="contained" color="primary">
-          Start Your Search
-        </Button>
-      </Link>
+      <Button
+        component={Link}
+        to="/scholarships"
+        variant="contained"
+        color="primary">
+        Start Your Search
+      </Button>
     </Container>
   );
 }
