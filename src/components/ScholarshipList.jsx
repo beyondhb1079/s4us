115379838d2d7a70647bf89d8f55d819ff6745d8
Josue Rios <josuerios777@gmail.com>
2020-11-04 17:56:11 -0800

    Fetch data and style scholarships search page (#155)
    * Fetch real scholarships list and basic styling
    
    * Fixed styling and added share for mobile browsers
    
    * fix scholarships tests
    
    * better share message
    
    * circular progress
    
    * circular progress
    
    * useEffect just once for now

--- src/components/ScholarshipList.jsx
+++ src/components/ScholarshipList.jsx
@@ -1,43 +1,113 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Grid,
+  IconButton,
+  Link as MuiLink,
+  Typography,
+} from '@material-ui/core';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ShareIcon from '@material-ui/icons/Share';
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
+import { BRAND_NAME } from '../config/constants';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    maxWidth: '100%',
-    backgroundColor: theme.palette.background.paper,
+  actions: {
+    padding: theme.spacing(2),
+    paddingTop: theme.spacing(1),
   },
-  link: {
-    textDecoration: 'none',
+  content: {
+    paddingTop: theme.spacing(0),
+    paddingBottom: theme.spacing(1),
   },
-  item: {
-    color: '#000',
+  description: {
+    display: '-webkit-box',
+    '-webkit-line-clamp': 5,
+    lineClamp: 5,
+    '-webkit-box-orient': 'vertical',
+    overflow: 'hidden',
   },
 }));
 
-function ScholarshipList(props) {
+function ScholarshipList({ scholarships }) {
   const classes = useStyles();
-  const { scholarships } = props;
-  const scholarshipList = Object.entries(scholarships).map(([id, item]) => (
-    <Link to={`/scholarships/${id}`} key={id} className={classes.link}>
-      <ListItem divider alignItems="flex-start" className={classes.item}>
-        <ListItemText primary={item.name} secondary={item.school} />
-        <ListItemText primary={item.amount} />
-        <ListItemText primary={item.deadline} />
-      </ListItem>
-    </Link>
-  ));
-
-  return <List className={classes.root}>{scholarshipList}</List>;
+  const shareFn = (id, data) => () => {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: `${data.amount} - ${data.name}`,
+          url: `${window.location.hostname}/scholarships/${id}`,
+          text:
+            `${data.amount} - ${data.name}\n` +
+            `${data.deadline.toLocaleDateString()}\n` +
+            `Shared via ${BRAND_NAME} (under construction)`,
+        })
+        .then(() => console.log('Thanks for sharing!'))
+        .catch(console.error);
+    } else {
+      // TODO(https://github.com/beyondhb1079/s4us/issues/154): Share dialog for Web
+      alert('This feature is under construction');
+    }
+  };
+  return (
+    <Grid container spacing={3}>
+      {scholarships.map(({ id, data }) => (
+        <Grid item xs={12} key={id}>
+          <Card variant="outlined">
+            <CardActionArea component={Link} to={`/scholarships/${id}`}>
+              <CardHeader title={data.name} subheader={data.amount} />
+              <CardContent className={classes.content}>
+                <Typography gutterBottom variant="body1">
+                  Deadline:{' '}
+                  <Typography component="span" color="primary">
+                    {data.deadline.toLocaleDateString()}
+                  </Typography>
+                </Typography>
+                <Typography
+                  variant="body1"
+                  color="textSecondary"
+                  className={classes.description}>
+                  {data.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.actions}>
+              <Button
+                component={MuiLink}
+                href={data.website}
+                color="primary"
+                variant="contained">
+                Apply
+              </Button>
+              <IconButton
+                aria-label="add to bookmarks"
+                color="primary"
+                onClick={() => alert('This feature is under construction')}>
+                <BookmarkBorderIcon />
+              </IconButton>
+              <IconButton
+                aria-label="share"
+                color="primary"
+                onClick={shareFn(id, data)}>
+                <ShareIcon />
+              </IconButton>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
 ScholarshipList.propTypes = {
-  scholarships: PropTypes.objectOf(PropTypes.object),
+  scholarships: PropTypes.arrayOf(PropTypes.object),
 };
 ScholarshipList.defaultProps = {
   scholarships: [],
