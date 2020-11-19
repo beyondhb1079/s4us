a223f5d4a57bee1d277101dbad3938e6a474eb38
Josue Rios <josuerios777@gmail.com>
2020-11-19 06:24:44 -0800

    proper merge

--- src/pages/Scholarships.test.jsx
+++ src/pages/Scholarships.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import firebase from 'firebase';
 import { clearFirestoreData } from '@firebase/rules-unit-testing';
 import { MemoryRouter, Route } from 'react-router-dom';
