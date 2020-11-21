c2284a14c144a49bcde4c4bf6ee323898537be8f
Josue Rios <josuerios777@gmail.com>
2020-11-21 08:46:37 +0000

    setup Firebase just once

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import firebase from 'firebase';
-import React from 'react';
+import React, { createContext } from 'react';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import FirebaseConfig from './config/FirebaseConfig';
 import Footer from './components/Footer';
@@ -14,25 +14,49 @@ import Contact from './pages/Contact';
 import ScholarshipForm from './pages/ScholarshipForm';
 import theme from './theme';
 
+const FirebaseContext = createContext(null);
+
+function FirebaseProvider({ children }) {
+  if (firebase.apps.length === 0) {
+    switch (process.env.NODE_ENV) {
+      case 'production':
+        firebase.initializeApp(FirebaseConfig);
+        break;
+      // TODO: consider staging environment
+      case 'development':
+      case 'test':
+      default:
+        firebase.initializeApp(FirebaseConfig);
+        firebase.firestore().settings({ host: 'localhost:8080', ssl: false });
+      // TODO: do we want test environment too?
+    }
+  }
+  return (
+    <FirebaseContext.Provider value={true}>{children}</FirebaseContext.Provider>
+  );
+}
+
 function App() {
-  firebase.initializeApp(FirebaseConfig);
+  console.log(JSON.stringify(process.env));
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/scholarships/new" component={ScholarshipForm} />
-          <Route path="/scholarships/:id" component={ScholarshipDetails} />
-          <Route path="/scholarships" component={Scholarships} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/" component={Home} />
-        </Switch>
-        <Footer />
-      </Router>
-    </ThemeProvider>
+    <FirebaseProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/scholarships/new" component={ScholarshipForm} />
+            <Route path="/scholarships/:id" component={ScholarshipDetails} />
+            <Route path="/scholarships" component={Scholarships} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/" component={Home} />
+          </Switch>
+          <Footer />
+        </Router>
+      </ThemeProvider>
+    </FirebaseProvider>
   );
 }
 
