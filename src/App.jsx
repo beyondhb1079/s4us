e6b7e3b0d2e682bb388f4fbac8e8e8f0c0b2270f
Josue Rios <josuerios777@gmail.com>
2020-11-21 20:20:51 -0800

    Create Firebase context provider that uses emulators on local development (#215)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import firebase from 'firebase';
 import React from 'react';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
-import FirebaseConfig from './config/FirebaseConfig';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -13,26 +11,27 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import ScholarshipForm from './pages/ScholarshipForm';
 import theme from './theme';
+import FirebaseProvider from './lib/FirebaseProvider';
 
 function App() {
-  firebase.initializeApp(FirebaseConfig);
-
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/scholarships/new" component={ScholarshipForm} />
-          <Route path="/scholarships/:id" component={ScholarshipDetails} />
-          <Route path="/scholarships" component={Scholarships} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/" component={Home} />
-        </Switch>
-        <Footer />
-      </Router>
-    </ThemeProvider>
+    <FirebaseProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/scholarships/new" component={ScholarshipForm} />
+            <Route path="/scholarships/:id" component={ScholarshipDetails} />
+            <Route path="/scholarships" component={Scholarships} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/" component={Home} />
+          </Switch>
+          <Footer />
+        </Router>
+      </ThemeProvider>
+    </FirebaseProvider>
   );
 }
 
