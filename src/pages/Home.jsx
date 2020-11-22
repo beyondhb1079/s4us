2b9f7c008d4fa1de1981795e2df20bcdc42abafb
Gonzalo Jr <64123425+gonzalo1330@users.noreply.github.com>
2020-11-21 23:03:01 -0800

    Scholarships made simple section (#89)
    * auth for firebase and home page
    
    * Delete package-lock.json
    
    * Delete hosting.YnVpbGQ.cache
    
    * Update .gitignore
    
    * Updated
    
    * use signInSuccessWithAuthResult
    
    * try to disable redirect on email login
    
    * fix merge conflict
    
    * updating package.json
    
    * Fixed lint errors
    
    * 9/14 review updates, lint error only 1 error left
    
    * changes export and import names
    
    * zero lint errors
    
    * fixed homepage login dialog and commented out app.test
    
    * passed all test
    
    * changes from 9/16 review
    
    * yarn.lock changed
    
    * fixed build
    
    * fixed build
    
    * fixed home login button bug
    
    * updated LoginDialog to include firebase auth
    
    * signin/signout button fixed
    
    * all lint errors fixed
    
    * updated 9/22 review requests
    
    * update LoginDialog class to a function
    
    * start of fetching data from firestore
    
    * working fetch
    
    * home stepper low details
    
    * updated the collection of user info for profile
    
    * redirect when stepper is complete & include 2 selections for scholarships details step
    
    * fixed lint errors from files for home-stepper
    
    * initial home page styling based on figma design prototype
    
    * implemented some functionality for Organization's interacting in our home page and low detail styling
    
    * updating homepage
    
    * redirect to stepper page when successfully logged in
    
    * selection of profile
    
    * updated the stepper to community contributor
    
    * updated the selection when user selects community contributor on the profile selection in the stepper
    
    * fixed lint and test issues
    
    * redirect to /newuser when successfully logged in
    
    * updating branch
    
    * created a new branch for profile display in home page
    
    * Low detail home page grid
    
    * fixed the image inside a button bug
    
    * incorporated changes based on review comments
    
    * incorporated review changes and fixed lint errors
    
    * ScholarshipMadeSimpleGrid content switch implemented
    
    * updated img to match figma design
    
    * implemented majority of the revision request changes
    
    * fixed <Grid> issues
    
    * did a modification to the grid sizing
    
    * fixed requested changed
    
    * lint issues fixed
    
    * fixed redirect when contributor is seleted
    
    * generic home section component
    
    * generic home section component
    
    * fixed home style
    
    * lint fixed
    
    * restored LoginDialog
    
    Co-authored-by: Josue Rios <josuerios777@gmail.com>
    Co-authored-by: Gonzalo Lara <gonzalomac@Gonzalos-MacBook-Pro.local>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { makeStyles, Typography } from '@material-ui/core';
+import ScholarshipsMadeSimple from '../components/ScholarshipsMadeSimple';
+import HomeSection from '../components/HomeSection';
+import pic from '../img/blank.png';
+
+const useStyles = makeStyles(() => ({
+  description: {
+    textAlign: 'center',
+  },
+}));
 
 function Home() {
+  const classes = useStyles();
   return (
-    <Container style={{ textAlign: 'center' }}>
-      <h1>Find Scholarships Today</h1>
-    </Container>
+    <>
+      <Typography variant="h2" className={classes.description} gutterBottom>
+        Find Scholarships Today
+      </Typography>
+      <ScholarshipsMadeSimple />
+      <HomeSection
+        alignItems="center"
+        direction="row-reverse"
+        title="For and by the Community"
+        description="Et has minim eltir intellegat. Mea aeterno elefiend antiopam ad, nam no suscipit quaerendum. At name mininum ponderum. Est audiam animal molestiate te."
+        buttons={[
+          <Button
+            component={Link}
+            to="/about"
+            variant="outlined"
+            color="primary">
+            Learn More
+          </Button>,
+        ]}
+        pic={pic}
+      />
+    </>
   );
 }
 
