90d0664dee4e7c6ab5b502299eddef8ad459a267
Josue Rios <josuerios777@gmail.com>
2020-11-21 22:55:01 -0800

    Set page titles (#211)
    * Set the default page title
    
    * Update App.jsx
    
    * fixed lint
    
    * use effects
    
    * Add page titles everywhere
    
    * hmm
    
    * style
    
    * Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -11,9 +12,59 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import ScholarshipForm from './pages/ScholarshipForm';
 import theme from './theme';
+import { BRAND_NAME } from './config/constants';
 import FirebaseProvider from './lib/FirebaseProvider';
 
+function RouteWithTitle({ path, component, title }) {
+  useEffect(() => {
+    document.title = `${BRAND_NAME} | ${title}`;
+  }, [title]);
+  return <Route {...{ path, component }} />;
+}
+RouteWithTitle.propTypes = {
+  component: PropTypes.node.isRequired,
+  path: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+const routes = [
+  {
+    path: '/scholarships/new',
+    title: 'Add a scholarship',
+    component: ScholarshipForm,
+  },
+  {
+    path: '/scholarships/:id',
+    title: 'Details',
+    component: ScholarshipDetails,
+  },
+  {
+    path: '/scholarships',
+    title: 'Scholarships',
+    component: Scholarships,
+  },
+  {
+    path: '/about',
+    title: 'About',
+    component: About,
+  },
+  {
+    path: '/contact',
+    title: 'Contact',
+    component: Contact,
+  },
+  {
+    path: '/',
+    title: 'Home',
+    component: Home,
+  },
+];
+
 function App() {
+  useEffect(() => {
+    document.title = `${BRAND_NAME} | Scholarships for Undocumented Students`;
+  }, []);
+
   return (
     <FirebaseProvider>
       <ThemeProvider theme={theme}>
@@ -21,12 +72,9 @@ function App() {
         <Router>
           <Header />
           <Switch>
-            <Route path="/scholarships/new" component={ScholarshipForm} />
-            <Route path="/scholarships/:id" component={ScholarshipDetails} />
-            <Route path="/scholarships" component={Scholarships} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/" component={Home} />
+            {routes.map(({ path, component, title }) => (
+              <RouteWithTitle {...{ path, component, title }} />
+            ))}
           </Switch>
           <Footer />
         </Router>
