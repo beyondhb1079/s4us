0ccb3b76a0c741e7440e24e883229dc9080af341
gonzalo1@uw.edu <gonzalo1@uw.edu>
2020-11-23 10:07:15 -0800

    menu list styling

--- src/components/ProfileDropdown.jsx
+++ src/components/ProfileDropdown.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import {
   Avatar,
   Container,
@@ -16,7 +16,6 @@ import NewIcon from '@material-ui/icons/NewReleases';
 import BookmarkIcon from '@material-ui/icons/Bookmark';
 import DoneIcon from '@material-ui/icons/Done';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import { makeStyles } from '@material-ui/core/styles';
 
 // hacky way to override Menu style
 const StyledMenu = withStyles({
@@ -28,6 +27,14 @@ const StyledMenu = withStyles({
 })(Menu);
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: '10px',
+    marginBottom: '15px',
+  },
+  menuList: {
+    marginTop: '10px',
+    marginBottom: '10px',
+  },
   profilePic: {
     alignSelf: 'center',
   },
@@ -56,20 +63,15 @@ export default function ProfileDropdown(props) {
 
   return (
     <Container>
-      <Avatar
-        alt="Remy Sharp"
-        src={photo}
-        onClick={handleClick}
-        className={classes.large}
-      />
+      <Avatar src={photo} onClick={handleClick} className={classes.large} />
       <StyledMenu
         keepMounted
         anchorEl={dropMenu}
         open={Boolean(dropMenu)}
         onClose={handleClose}>
-        <Grid container spacing={2}>
+        <Grid container spacing={2} className={classes.root}>
           <Grid item xs={3} sm={3} className={classes.profilePic}>
-            <Avatar alt="Remy Sharp" src={photo} className={classes.medium} />
+            <Avatar src={photo} className={classes.medium} />
           </Grid>
           <Grid item xs={9} sm={9}>
             <Typography variant="h6" component="h4" gutterBottom>
@@ -81,27 +83,27 @@ export default function ProfileDropdown(props) {
             <a href="/home">Manage Your Profile</a>
           </Grid>
         </Grid>
-        <Divider light />
-        <MenuItem>
+        <Divider />
+        <MenuItem className={classes.menuList}>
           <ListItemIcon>
             <NewIcon fontSize="small" />
           </ListItemIcon>
           <ListItemText primary="New" />
         </MenuItem>
-        <MenuItem>
+        <MenuItem className={classes.menuList}>
           <ListItemIcon>
             <BookmarkIcon fontSize="small" />
           </ListItemIcon>
           <ListItemText primary="Saved" />
         </MenuItem>
-        <MenuItem>
+        <MenuItem className={classes.menuList}>
           <ListItemIcon>
             <DoneIcon fontSize="small" />
           </ListItemIcon>
           <ListItemText primary="Applied" />
         </MenuItem>
-        <Divider light />
-        <MenuItem onClick={signOut}>
+        <Divider />
+        <MenuItem onClick={signOut} className={classes.menuList}>
           <ListItemIcon>
             <ExitToAppIcon fontSize="small" />
           </ListItemIcon>
