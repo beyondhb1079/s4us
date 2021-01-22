5d3d5caa230337ef7286d73126fcfbb308f6b14b
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2021-01-22 00:11:38 -0800

    styling implementation

--- src/components/AmountFilter.jsx
+++ src/components/AmountFilter.jsx
@@ -1,29 +1,20 @@
 import React, { useState } from 'react';
-import { Button, Popover, InputLabel } from '@material-ui/core';
+import { Button, Popover, InputLabel, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import RemoveIcon from '@material-ui/icons/Remove';
 import AmountTextField from './AmountTextField';
 
 const useStyles = makeStyles((theme) => ({
   buttonStyle: {
     margin: theme.spacing(1),
-    minWidth: 120,
+    maxWidth: 120,
     height: 57,
   },
+  labelStyle: { paddingLeft: theme.spacing(1) },
+  dashStyle: { margin: '45px auto auto' },
+  filterStyle: { margin: '10px 0' },
 }));
-
-function displayFilter(label, value, onChange) {
-  return (
-    <>
-      <InputLabel>{label}</InputLabel>
-      <AmountTextField
-        disabled={false}
-        variation="outlined"
-        {...{ value, onChange }}
-      />
-    </>
-  );
-}
 
 export default function AmountFilter() {
   const classes = useStyles();
@@ -34,6 +25,19 @@ export default function AmountFilter() {
 
   const handleChange = (index, val) =>
     setValues({ ...values, [index]: parseInt(val, 10) || '' });
+
+  function displayFilter(label, value, onChange) {
+    return (
+      <Grid item className={classes.filterStyle}>
+        <InputLabel className={classes.labelStyle}>{label}</InputLabel>
+        <AmountTextField
+          disabled={false}
+          variation="outlined"
+          {...{ value, onChange }}
+        />
+      </Grid>
+    );
+  }
 
   return (
     <>
@@ -50,13 +54,18 @@ export default function AmountFilter() {
         anchorEl={anchorEl}
         onClose={() => setAnchorEl(null)}
         anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'left' }}>
-        {displayFilter('Min Amount', values.min, (e) =>
-          handleChange('min', e.target.value)
-        )}
-        {displayFilter('Max Amount', values.max, (e) =>
-          handleChange('max', e.target.value)
-        )}
+        transformOrigin={{ vertical: -5, horizontal: 'left' }}>
+        <Grid container>
+          {displayFilter('Min Amount', values.min, (e) =>
+            handleChange('min', e.target.value)
+          )}
+          <Grid item className={classes.dashStyle}>
+            <RemoveIcon />
+          </Grid>
+          {displayFilter('Max Amount', values.max, (e) =>
+            handleChange('max', e.target.value)
+          )}
+        </Grid>
       </Popover>
     </>
   );
