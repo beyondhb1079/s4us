edf201b67b6c5d33c3854eccc157ed4345ae9973
Job Henandez Lara <69167740+Jobhdez@users.noreply.github.com>
2021-01-26 23:52:06 -0800

    Created Job's summary [issue 347] (#349)
    * Created Job's summary [issue 347]
    
    * fixed identation
    
    * fixed indentation issue
    
    * fixed indentation issue
    
    * Fixed indentation
    
    * fixed indentation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -58,8 +58,18 @@ const team = [
     img: testPic,
     description: <>Todo: about Alejandro Loaiza</>,
   },
+  {
+    name: 'Job Hernandez',
+    img: testPic,
+    description: (
+      <>
+        I am driven to solve problems with software; software has tremendous
+        power to transform society. So, I would like to contribute and have an
+        impact.
+      </>
+    ),
+  },
 ];
-
 function About() {
   return (
     <Container>
@@ -72,7 +82,6 @@ function About() {
         Ullamco laboris ea ut cupidatat non velit mollit ea ullamco eiu. Enim
         amet officia fugiat dolore ullamco consequat laboris culpa.
       </p>
-
       <h2>Our History</h2>
       <p>
         Et dolor ex pariatur ex voluptate commodo voluptate culpa commodo
@@ -81,7 +90,6 @@ function About() {
         cillum tempor elit proident. Esse culpa eiusmod irure aute qui nulla.
         Reprehenderit enim aute commodo cupidatat ut ipsum ea minim magna velit.
       </p>
-
       <h2>How {BRAND_NAME} Works</h2>
       <p>
         Laboris amet nostrud amet anim fugiat deserunt ad laboris dolore sint
@@ -90,7 +98,6 @@ function About() {
         pariatur occaecat aliqua reprehenderit laboris. Officia qui ad do magna
         excepteur proident laborum ea laborum consectetur aliqua pariatur.
       </p>
-
       <h2>Meet the Team</h2>
       <Grid container justify="flex-start" alignItems="stretch" spacing={3}>
         {team.map(({ name, img, description }) => (
@@ -99,7 +106,6 @@ function About() {
           </Grid>
         ))}
       </Grid>
-
       <h2>Apply for Scholarships today</h2>
       <p>
         Laborum aliqua labore nulla non consequat mollit nostrud quis commodo
@@ -115,5 +121,4 @@ function About() {
     </Container>
   );
 }
-
 export default About;
