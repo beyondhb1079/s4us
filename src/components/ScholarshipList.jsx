c22a1c8bb91164234500e47fa1165e2f418af393
Oat Bo <aotsincere@gmail.com>
2021-02-06 00:46:08 +0700

    move shareFn func to be the same as before.

--- src/components/ScholarshipList.jsx
+++ src/components/ScholarshipList.jsx
@@ -6,40 +6,33 @@ import ShareDialog from './ShareDialog';
 
 import ScholarshipListCard from './ScholarshipListCard';
 
-// move out from component
-// because it don't need to recreate every time component render
-const shareFn = (
-  id,
-  data,
-  setShareSiteLink,
-  setShareSiteTitle,
-  setShareDialogOpen
-) => () => {
-  const title = `${data.amount} - ${data.name} | ${BRAND_NAME}`;
-  const url = `https://${window.location.hostname}/scholarships/${id}`;
-  const text =
-    `${data.amount} - ${data.name} | ${BRAND_NAME}\n` +
-    `${data.deadline.toLocaleDateString()}\n`;
-  if (navigator.share) {
-    navigator
-      .share({ title, url, text })
-      // eslint-disable-next-line no-console
-      .then(() => console.log('Thanks for sharing!'))
-      // eslint-disable-next-line no-console
-      .catch(console.error);
-  } else {
-    setShareSiteLink(url);
-    setShareSiteTitle(title);
-    setShareDialogOpen(true);
-  }
-};
-
 function ScholarshipList({ scholarships }) {
   const [shareDialogOpen, setShareDialogOpen] = useState(false);
   const closeShareDialog = () => setShareDialogOpen(false);
 
   const [shareSiteLink, setShareSiteLink] = React.useState('');
   const [shareSiteTitle, setShareSiteTitle] = React.useState('');
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const shareFn = (id, data) => () => {
+    const title = `${data.amount} - ${data.name} | ${BRAND_NAME}`;
+    const url = `https://${window.location.hostname}/scholarships/${id}`;
+    const text =
+      `${data.amount} - ${data.name} | ${BRAND_NAME}\n` +
+      `${data.deadline.toLocaleDateString()}\n`;
+    if (navigator.share) {
+      navigator
+        .share({ title, url, text })
+        // eslint-disable-next-line no-console
+        .then(() => console.log('Thanks for sharing!'))
+        // eslint-disable-next-line no-console
+        .catch(console.error);
+    } else {
+      setShareSiteLink(url);
+      setShareSiteTitle(title);
+      setShareDialogOpen(true);
+    }
+  };
 
   return (
     <Grid container spacing={3}>
