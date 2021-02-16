6ffe86ee03469c43bcc0abafa6ae2774c77f2188
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2021-02-15 20:33:16 -0800

    Amount filter functionality & URL support (#355)
    * 'unknown' label show & initial filter validation
    
    * amount filter validation & helperText styling
    
    * Amount validation to allow min=max & api delay setup
    
    * initial filter functionality solution
    
    * refactor filter by amount logic
    
    * URL parameter support
    
    * cleaner filter logic & invalid URL parameter check
    
    * Filter logic changes, prune invalid URL parameters & other suggest changes
    
    * query support refactoring, filter logic simplified & other suggestions
    
    * cleanup
    
    * delete param moved to FilterBar, simplified filter logic & other suggest changes
    
    * suggest changes
    
    * minor changes

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { makeStyles, Grid } from '@material-ui/core';
 import FilterDropDown from './FilterDropdown';
 import AmountFilter from './AmountFilter';
+import qParams from '../lib/QueryParams';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -37,8 +38,10 @@ const sortingOptions = {
 };
 
 export default function FilterBar(props) {
-  const { changeSortBy, changeSortFormat } = props;
+  const { changeSortBy, changeSortFormat, setQueryParam, queryParams } = props;
   const classes = useStyles();
+
+  const { minAmount, maxAmount } = queryParams;
 
   function updateSorting(sortingOption) {
     switch (sortingOption) {
@@ -67,7 +70,12 @@ export default function FilterBar(props) {
       <Grid item className={classes.alignText}>
         <FilterDropDown label="Major" items={majors} />
         <FilterDropDown label="Grade" items={grades} />
-        <AmountFilter />
+        <AmountFilter
+          min={minAmount ?? 0}
+          max={maxAmount ?? 0}
+          onMinChange={(e) => setQueryParam(qParams.MIN_AMOUNT, e.target.value)}
+          onMaxChange={(e) => setQueryParam(qParams.MAX_AMOUNT, e.target.value)}
+        />
       </Grid>
       <Grid item className={classes.alignText}>
         Sort by
@@ -86,4 +94,9 @@ export default function FilterBar(props) {
 FilterBar.propTypes = {
   changeSortBy: PropTypes.func.isRequired,
   changeSortFormat: PropTypes.func.isRequired,
+  setQueryParam: PropTypes.func.isRequired,
+  queryParams: PropTypes.objectOf(PropTypes.number),
+};
+FilterBar.defaultProps = {
+  queryParams: {},
 };
--- src/pages/Scholarships.jsx
+++ src/pages/Scholarships.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import queryString from 'query-string';
 import {
   CircularProgress,
   Container,
@@ -9,6 +11,7 @@ import Button from '@material-ui/core/Button';
 import Scholarships from '../models/Scholarships';
 import ScholarshipList from '../components/ScholarshipList';
 import FilterBar from '../components/FilterBar';
+import qParams from '../lib/QueryParams';
 
 const useStyles = makeStyles((theme) => ({
   progress: {
@@ -27,13 +30,58 @@ function ScholarshipsPage() {
   const [sortField, setSortField] = useState('deadline');
   const [sortDir, setSortDir] = useState('asc');
 
+  const location = useLocation();
+  const history = useHistory();
+
+  const params = queryString.parse(location.search, { parseNumbers: true });
+  const { minAmount, maxAmount } = params;
+
+  const setQueryParam = (index, val) => {
+    history.push({
+      search: queryString.stringify({
+        ...params,
+        [index]: val,
+      }),
+    });
+  };
+
+  const pruneQueryParam = (index) => {
+    delete params[index];
+    history.replace({ search: queryString.stringify(params) });
+  };
+
+  if (
+    minAmount !== undefined &&
+    !(Number.isInteger(minAmount) && minAmount > 0)
+  ) {
+    pruneQueryParam(qParams.MIN_AMOUNT);
+  }
+
+  if (
+    maxAmount !== undefined &&
+    !(Number.isInteger(maxAmount) && maxAmount > 0)
+  ) {
+    pruneQueryParam(qParams.MAX_AMOUNT);
+  }
+
   useEffect(() => {
+    if (maxAmount && maxAmount < minAmount) {
+      setError('The minimum amount must be less than the Maximum.');
+      return;
+    }
     // TODO: Create cancellable promises
     Scholarships.list({ sortField, sortDir })
-      .then(setScholarships)
+      .then((results) =>
+        setScholarships(
+          results.filter((s) =>
+            s.data.amount.intersectsRange(minAmount, maxAmount)
+          )
+        )
+      )
+      .then(() => setError(null))
       .catch(setError)
       .finally(() => setLoading(false));
-  }, [sortDir, sortField]);
+  }, [sortDir, sortField, minAmount, maxAmount]);
 
   const classes = useStyles();
 
@@ -42,7 +90,12 @@ function ScholarshipsPage() {
       <Typography variant="h3" component="h1" style={{ textAlign: 'center' }}>
         Scholarships
       </Typography>
-      <FilterBar changeSortBy={setSortField} changeSortFormat={setSortDir} />
+      <FilterBar
+        changeSortBy={setSortField}
+        changeSortFormat={setSortDir}
+        queryParams={params}
+        {...{ setQueryParam }}
+      />
       {error?.toString() ||
         (loading && <CircularProgress className={classes.progress} />) || (
           <>
