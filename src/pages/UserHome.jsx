cc381d7c4c4cdd0ab070dc13caf2f5a589c08c91
Job <hj93@protonmail.com>
2021-03-07 16:49:31 -0800

    added an equal sign.

--- src/pages/UserHome.jsx
+++ src/pages/UserHome.jsx
@@ -113,7 +113,7 @@ export default function UserHome() {
           component="h2"
           gutterBottom
           className={classes.noScholarshipsAdded}>
-          <div>{scholarships.length == 0 && 'No Scholarships added.'}</div>
+          <div>{scholarships.length === 0 && 'No Scholarships added.'}</div>
         </Typography>
 
         <Typography component="h2" color="primary">
