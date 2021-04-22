0bafb55cacd6261f704529dd606625f9f8047add
Gonzalo Jr <64123425+gonzalo1330@users.noreply.github.com>
2021-04-21 19:31:34 -0700

    grade selection (#380)

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { makeStyles, Grid } from '@material-ui/core';
 import FilterDropDown from './FilterDropdown';
 import AmountFilter from './AmountFilter';
+import GradeLevelFilter from './GradeLevelFilter';
 import qParams from '../lib/QueryParams';
 
 const useStyles = makeStyles(() => ({
@@ -20,14 +21,6 @@ const majors = {
   che: 'Chemical Eng.',
   cs: 'Computer Science',
   ds: 'Data Science',
-};
-
-const grades = {
-  12: 'Highschool Senior',
-  13: 'College Freshman',
-  14: 'College Sophomore',
-  15: 'College Junior',
-  16: 'College Senior',
 };
 
 const sortOptions = {
@@ -70,7 +63,7 @@ export default function FilterBar(props) {
     <Grid container spacing={2} className={classes.root}>
       <Grid item className={classes.alignText}>
         <FilterDropDown label="Major" items={majors} />
-        <FilterDropDown label="Grade" items={grades} />
+        <GradeLevelFilter />
         <AmountFilter
           min={minAmount ?? 0}
           max={maxAmount ?? 0}
