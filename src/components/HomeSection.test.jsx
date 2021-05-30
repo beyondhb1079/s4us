87862a285ab4a8b13e1344c8eae9eaee98b8414d
Job <hj93@protonmail.com>
2021-05-29 17:23:01 -0700

    added a rendering test.

---
+++ src/components/HomeSection.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import HomeSection from './HomeSection';
+
+describe('HomeSection', () => {
+    test('renders HomeSection component', () => {
+        render(<HomeSection
+                 title="Topology"
+                 description="Math course"
+                 buttons={[]}
+                 pic="manifold"/>);
+    });
+});
