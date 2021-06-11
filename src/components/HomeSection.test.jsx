2cccd8a0a682ba1b22d059fb9ade38036adf52d7
Job <hj93@protonmail.com>
2021-06-11 16:25:58 -0700

    Added tests to check for title, description, and number of buttons.

--- src/components/HomeSection.test.jsx
+++ src/components/HomeSection.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-
+import { render, screen } from '@testing-library/react';
+import { initializeTestApp } from '../lib/testing';
 import HomeSection from './HomeSection';
 
 describe('HomeSection', () => {
-  test('renders HomeSection component', () => {
+  test('checks if the title is in the document', () => {
     render(
       <HomeSection
         title="Topology"
@@ -13,5 +13,33 @@ describe('HomeSection', () => {
         pic="manifold"
       />
     );
+    screen.debug();
+    expect(screen.getByText('Topology')).toBeInTheDocument();
+  });
+
+  test('checks if the description is in the document', () => {
+    render(
+      <HomeSection
+        title="Topology"
+        description="Math course"
+        buttons={[]}
+        pic="manifold"
+      />
+    );
+    expect(screen.getByText('Math course')).toBeInTheDocument();
+  });
+
+  test('checks if it renders the given number of buttons', () => {
+    render(
+      <HomeSection
+        title="Topology"
+        description="Math course"
+        buttons={[]}
+        pic="manifold"
+      />
+    );
+    screen.debug();
+    const items = screen.queryByRole('button');
+    expect(items).toBe(null);
   });
 });
