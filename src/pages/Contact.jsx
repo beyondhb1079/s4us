d737efdda026fb05beee6ede3e1f072970ae9b12
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2021-06-24 15:28:47 -0700

    pre-populate mailto with device info

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import {
+  isMobile,
+  osVersion,
+  osName,
+  fullBrowserVersion,
+  browserName,
+  deviceType,
+  mobileVendor,
+  mobileModel,
+} from 'react-device-detect';
 import Container from '@material-ui/core/Container';
 import { Link } from 'react-router-dom';
 import { BRAND_NAME } from '../config/constants';
 
 function Contact() {
+  const mobileInfo = `Device Type: ${deviceType} Vendor: ${mobileVendor} Model: ${mobileModel}`;
+  const deviceInfo = `OS Name: ${osName}%0AOSVersion: ${osVersion}%0ABrowser: ${browserName}%0ABrowser Version: ${fullBrowserVersion}%0A${
+    isMobile ? mobileInfo : ''
+  }`;
+
+  function emailLink(LinkText) {
+    return (
+      <a
+        href={`mailto:dreamscholars-contact@googlegroups.com?subject=Bug%20Report&body=${deviceInfo}`}>
+        {LinkText}
+      </a>
+    );
+  }
+
   return (
     <Container>
+      <h1>{deviceInfo}</h1>
       <h1 style={{ textAlign: 'center' }}>Contact Us</h1>
 
       <h3>Check Our FAQ</h3>
@@ -15,10 +40,7 @@ function Contact() {
       </p>
 
       <h3>Found a bug</h3>
-      <p>
-        If you found a bug you can file it{' '}
-        <a href="https://github.com/beyondhb1079/s4us/issues">here</a> .
-      </p>
+      <p>If you found a bug you can file it {emailLink('here')}</p>
 
       <h3>Learn About Us</h3>
       <p>
@@ -29,8 +51,10 @@ function Contact() {
       <h3>Reach Out</h3>
       <p>
         We are happy to answer any of your questions. You can email us{' '}
-        <a href="mailto:undocuscholar@googlegroups.com">here</a> and we&apos;ll
-        be in touch as soon as possible.
+        <a href="mailto:dreamscholars-contact@googlegroups.com?subject=Feature&body=">
+          here
+        </a>{' '}
+        and we&apos;ll be in touch as soon as possible.
       </p>
     </Container>
   );
