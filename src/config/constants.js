d9ba90278b682b6f5ac037d21283ec6a8be8b496
Josue Rios <josuerios777@gmail.com>
2021-06-30 09:37:47 -0700

    use experiments library for showing full profile menu (#506)
    * use experiments library for showing full profile menu
    
    * fix build

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export const BRAND_NAME = 'DreamScholars';
 export const SUBSCRIPTION_FORM_URL = 'https://forms.gle/qCpPA34L7ZNVpGH27';
-const params = new URLSearchParams(window.location.search);
-export const EXPERIMENT_SHOW_FULL_PROFILE_MENU =
-  params.get('experimentShowFullProfileMenu') === '1';
