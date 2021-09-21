49d26b067a53a2bdebc249cd8a37edf7ee3734d0
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2021-09-21 14:01:55 -0700

    Form general section styling (#613)
    * stepper implementation
    
    * award amount Select implementation
    
    * styling
    
    * mobile design
    
    * style fix
    
    * small fix
    
    * fixed some prop errors
    
    * suggested changes
    
    * fixed failing tests
    
    * small fix
    
    * fixed failing DatePicker tests
    
    * got rid of empty string if helperText null
    
    * small styling fixes
    
    * stepper buttons styling
    
    * DatePicker same length as other fields
    
    * ScholarshipAmountField suggested changes
    
    * suggested changes
    
    * back button when on last Stepper section

--- src/components/ScholarshipAmountField.jsx
+++ src/components/ScholarshipAmountField.jsx
@@ -1,44 +1,46 @@
 import React from 'react';
 import {
-  FormLabel,
-  Radio,
-  RadioGroup,
-  FormControl,
-  FormControlLabel,
+  InputLabel,
+  Grid,
+  Select,
+  MenuItem,
   FormHelperText,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import RemoveIcon from '@material-ui/icons/Remove';
 import PropTypes from 'prop-types';
 import AmountType from '../types/AmountType';
 import AmountTextField from './AmountTextField';
 
-const useStyles = makeStyles({
-  amountFieldStyle: {
-    display: 'flex',
-    alignItems: 'center',
+const useStyles = makeStyles(() => ({
+  amountSelect: {
+    minWidth: 150,
   },
-});
+  dash: {
+    height: '100%',
+    textAlign: 'center',
+  },
+}));
 
 function ScholarshipAmountField(props) {
   const classes = useStyles();
   const {
-    className,
     helperText,
     amountType,
     minAmount,
     maxAmount,
     onTypeChange,
     updateAmount,
+    labelStyle,
   } = props;
 
   const isRange = amountType === AmountType.Range;
   const isFixed = amountType === AmountType.Fixed;
   const error = !!helperText; // no error if helperText empty
 
-  const labels = {};
-  labels[AmountType.Range] = (
-    <div className={classes.amountFieldStyle}>
-      Range:
+  const inputFields = {};
+  inputFields[AmountType.Range] = (
+    <>
       <AmountTextField
         error={isRange && error && (!!maxAmount || minAmount >= maxAmount)}
         value={minAmount || ''}
@@ -48,7 +50,9 @@ function ScholarshipAmountField(props) {
         }}
         disabled={!isRange}
       />
-      to
+
+      <RemoveIcon className={classes.dash} />
+
       <AmountTextField
         error={isRange && error && !minAmount && !maxAmount}
         value={maxAmount || ''}
@@ -58,56 +62,59 @@ function ScholarshipAmountField(props) {
         }}
         disabled={!isRange}
       />
-    </div>
+    </>
   );
-  labels[AmountType.Fixed] = (
-    <div className={classes.amountFieldStyle}>
-      Fixed Amount:
-      <AmountTextField
-        error={isFixed && error}
-        value={minAmount || ''}
-        onChange={(e) => {
-          const val = parseInt(e.target.value, 10);
-          updateAmount(val || 0, val || 0);
-        }}
-        disabled={!isFixed}
-      />
-    </div>
+  inputFields[AmountType.Fixed] = (
+    <AmountTextField
+      error={isFixed && error}
+      value={minAmount || ''}
+      onChange={(e) => {
+        const val = parseInt(e.target.value, 10);
+        updateAmount(val || 0, val || 0);
+      }}
+      disabled={!isFixed}
+    />
   );
-  labels[AmountType.FullTuition] = 'Full Tuition';
 
   return (
-    <FormControl error={error} className={className}>
-      <FormLabel>Amount Type *</FormLabel>
-      <RadioGroup value={amountType} onChange={onTypeChange}>
-        {Object.values(AmountType).map((type) => (
-          <FormControlLabel
-            key={type}
-            value={type}
-            control={<Radio />}
-            label={labels[type] || 'Unknown'}
-          />
-        ))}
-      </RadioGroup>
-      <FormHelperText error>{helperText || ' '}</FormHelperText>
-    </FormControl>
+    <>
+      <InputLabel className={labelStyle}>Award Amount *</InputLabel>
+      <Grid container spacing={3}>
+        <Grid item>
+          <Select
+            className={classes.amountSelect}
+            variant="outlined"
+            value={amountType}
+            onChange={onTypeChange}
+            displayEmpty>
+            <MenuItem value={AmountType.Unknown}>Unknown</MenuItem>
+            <MenuItem value={AmountType.Fixed}>Fixed</MenuItem>
+            <MenuItem value={AmountType.Range}>Range</MenuItem>
+            <MenuItem value={AmountType.FullTuition}>Full Tuition</MenuItem>
+          </Select>
+        </Grid>
+
+        <Grid item>{amountType && inputFields[amountType]}</Grid>
+      </Grid>
+      <FormHelperText error>{helperText}</FormHelperText>
+    </>
   );
 }
 
 ScholarshipAmountField.propTypes = {
-  className: PropTypes.string,
   amountType: PropTypes.oneOf(Object.values(AmountType)),
   minAmount: PropTypes.number,
   maxAmount: PropTypes.number,
   onTypeChange: PropTypes.func.isRequired,
   updateAmount: PropTypes.func.isRequired,
   helperText: PropTypes.string,
+  labelStyle: PropTypes.string,
 };
 ScholarshipAmountField.defaultProps = {
-  className: null,
-  amountType: null,
+  amountType: undefined,
   helperText: '',
   minAmount: 0,
   maxAmount: 0,
+  labelStyle: '',
 };
 export default ScholarshipAmountField;
--- src/components/ScholarshipForm.jsx
+++ src/components/ScholarshipForm.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik, getIn } from 'formik';
-import { Button } from '@material-ui/core';
+import {
+  Button,
+  Stepper,
+  Step,
+  StepLabel,
+  StepContent,
+  Grid,
+  Typography,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import validationSchema from '../validation/ValidationSchema';
 import ScholarshipAmountField from './ScholarshipAmountField';
 import DatePicker from './DatePicker';
 import FormikTextField from './FormikTextField';
+import AmountType from '../types/AmountType';
 
 const useStyles = makeStyles((theme) => ({
-  containerStyle: {
-    maxWidth: 400,
-  },
-  fieldStyle: {
-    paddingBottom: theme.spacing(1),
-  },
-  submitStyle: {
+  stepperDescription: {
     marginBottom: theme.spacing(2),
+  },
+  inputLabel: {
+    marginBottom: theme.spacing(2),
+  },
+  stepperBtns: {
+    marginTop: theme.spacing(4),
   },
 }));
 
 function ScholarshipForm({ scholarship, submitFn, onSubmitError }) {
   const classes = useStyles();
+  const [activeStep, setActiveStep] = useState(0);
 
   const formik = useFormik({
     initialValues: scholarship.data,
@@ -43,67 +53,120 @@ function ScholarshipForm({ scholarship, submitFn, onSubmitError }) {
     formik.setFieldValue('amount.max', max, true);
   }
 
-  return (
-    <form className={classes.containerStyle} onSubmit={formik.handleSubmit}>
-      <div>
-        <FormikTextField label="Scholarship Name *" id="name" formik={formik} />
-      </div>
+  const stepperItems = {};
+  stepperItems.General = (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Typography className={classes.stepperDescription}>
+          Please add the general information about the scholarship.
+        </Typography>
+      </Grid>
 
-      <DatePicker
-        className={classes.fieldStyle}
-        id="deadline"
-        label="Deadline *"
-        error={formik.touched.deadline && Boolean(formik.errors.deadline)}
-        helperText={(formik.touched.deadline && formik.errors.deadline) || ' '}
-        value={formik.values.deadline}
-        onChange={(date) => formik.setFieldValue('deadline', date, true)}
-      />
-
-      <div>
+      <Grid item sm={6} xs={12}>
         <FormikTextField
-          label="Description *"
-          id="description"
+          label="Scholarship Name *"
+          id="name"
           formik={formik}
-          rows={8}
+          labelStyle={classes.inputLabel}
         />
-      </div>
+      </Grid>
 
-      <div>
+      <Grid item sm={6} xs={12}>
+        <FormikTextField
+          label="Organization"
+          id="organization"
+          formik={formik}
+          labelStyle={classes.inputLabel}
+        />
+      </Grid>
+
+      <Grid item sm={6} xs={12}>
         <FormikTextField
           label="Scholarship Link *"
           id="website"
           formik={formik}
+          labelStyle={classes.inputLabel}
         />
-      </div>
+      </Grid>
 
-      <ScholarshipAmountField
-        className={classes.fieldStyle}
-        helperText={
-          (getIn(formik.touched, 'amount.type') &&
-            getIn(formik.errors, 'amount.type')) ||
-          getIn(formik.errors, 'amount.min') ||
-          getIn(formik.errors, 'amount.max') ||
-          ''
-        }
-        amountType={formik.values.amount.type}
-        minAmount={formik.values.amount.min}
-        maxAmount={formik.values.amount.max}
-        onTypeChange={(e) =>
-          formik.setFieldValue('amount.type', e.target.value, true)
-        }
-        updateAmount={updateAmount}
-      />
+      <Grid item sm={6}>
+        <DatePicker
+          id="deadline"
+          label="Deadline *"
+          labelStyle={classes.inputLabel}
+          error={formik.touched.deadline && Boolean(formik.errors.deadline)}
+          helperText={formik.touched.deadline && formik.errors.deadline}
+          value={formik.values.deadline}
+          onChange={(date) => formik.setFieldValue('deadline', date, true)}
+        />
+      </Grid>
 
-      <div>
-        <Button
-          className={classes.submitStyle}
-          variant="contained"
-          color="primary"
-          type="submit"
-          disabled={formik.isSubmitting}>
-          Submit
-        </Button>
-      </div>
+      <Grid item>
+        <ScholarshipAmountField
+          helperText={
+            (getIn(formik.touched, 'amount.type') &&
+              getIn(formik.errors, 'amount.type')) ||
+            getIn(formik.errors, 'amount.min') ||
+            getIn(formik.errors, 'amount.max')
+          }
+          amountType={formik.values.amount.type ?? AmountType.Unknown}
+          minAmount={formik.values.amount.min}
+          maxAmount={formik.values.amount.max}
+          onTypeChange={(e) =>
+            formik.setFieldValue('amount.type', e.target.value, true)
+          }
+          updateAmount={updateAmount}
+          labelStyle={classes.inputLabel}
+        />
+      </Grid>
+
+      <Grid item xs={12}>
+        <FormikTextField
+          label="Description *"
+          id="description"
+          labelStyle={classes.inputLabel}
+          formik={formik}
+          minRows={8}
+        />
+      </Grid>
+    </Grid>
+  );
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {Object.keys(stepperItems).map((key) => (
+          <Step key={key}>
+            <StepLabel>{key}</StepLabel>
+            <StepContent>
+              {stepperItems[key]}
+              <div className={classes.stepperBtns}>
+                <Button
+                  disabled={activeStep === 0}
+                  onClick={() => setActiveStep((prevStep) => prevStep - 1)}>
+                  BACK
+                </Button>
+                {activeStep == Object.keys(stepperItems).length - 1 ? (
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    disabled={formik.isSubmitting}>
+                    Submit
+                  </Button>
+                ) : (
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => setActiveStep((prevStep) => prevStep + 1)}>
+                    NEXT
+                  </Button>
+                )}
+              </div>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
     </form>
   );
 }
