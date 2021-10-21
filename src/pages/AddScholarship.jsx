8e9d839865f69341e2dc8ca901e4f00302b83380
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2021-10-20 21:56:27 -0700

    initial implementation

--- src/pages/AddScholarship.jsx
+++ src/pages/AddScholarship.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import firebase from 'firebase';
-import { Alert, AlertTitle, Container, Typography } from '@mui/material';
+import {
+  Alert,
+  AlertTitle,
+  Container,
+  Typography,
+  Grid,
+  Card,
+} from '@mui/material';
 import ScholarshipForm from '../components/ScholarshipForm';
 import Scholarships from '../models/Scholarships';
 import SubmissionAlert from '../components/SubmissionAlert';
 import AmountType from '../types/AmountType';
+import { ReactComponent as BackgroundImg } from '../img/img3.svg';
 
 function AddScholarship() {
   const [submissionAlert, setSubmissionAlert] = useState(null);
@@ -32,31 +40,57 @@ function AddScholarship() {
       <Helmet>
         <title>Add a Scholarship</title>
       </Helmet>
-      <Typography variant="h4" gutterBottom>
-        Submit a Scholarship
-      </Typography>
-      <ScholarshipForm
-        scholarship={scholarship}
-        submitFn={() =>
-          setSubmissionAlert(
-            <SubmissionAlert
-              id={scholarship.id}
-              name={scholarship.data.name}
-              onClose={() => setSubmissionAlert(null)}
-            />
-          )
-        }
-        onSubmitError={(error) =>
-          setSubmissionAlert(
-            <Alert severity="error" onClose={() => setSubmissionAlert(null)}>
-              <AlertTitle>Error</AlertTitle>
-              {error.toString()}
-            </Alert>
-          )
-        }
-      />
 
-      {submissionAlert}
+      <Grid container>
+        <Grid item xs={12} sm={6}>
+          <Typography gutterBottom>Submit a Scholarship</Typography>
+          <Typography variant="h4" gutterBottom>
+            Additional information
+          </Typography>
+          <Typography>
+            Help our team provide the best search results for students and
+            community members looking for scholarships by contributing as much
+            detailed information possible.
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <BackgroundImg style={{ width: '120%', overflow: 'hidden' }} />
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        style={{
+          marginTop: '-80px',
+          zIndex: 5,
+          position: 'relative',
+          padding: '8px',
+        }}
+        component={Card}>
+        <ScholarshipForm
+          scholarship={scholarship}
+          submitFn={() =>
+            setSubmissionAlert(
+              <SubmissionAlert
+                id={scholarship.id}
+                name={scholarship.data.name}
+                onClose={() => setSubmissionAlert(null)}
+              />
+            )
+          }
+          onSubmitError={(error) =>
+            setSubmissionAlert(
+              <Alert severity="error" onClose={() => setSubmissionAlert(null)}>
+                <AlertTitle>Error</AlertTitle>
+                {error.toString()}
+              </Alert>
+            )
+          }
+        />
+
+        {submissionAlert}
+      </Grid>
     </Container>
   );
 }
