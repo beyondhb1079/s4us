158b011aef915b50674538bb3e5ef85568503896
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2021-10-22 16:52:08 -0700

    Grade/Ethnicity enum namespace (#691)
    * initial implementation
    
    * GradeLevel namespace implementation
    
    * ethnicity namespace
    
    * modify renderValue and fix Ethnicity values func

--- src/types/Ethnicity.ts
+++ src/types/Ethnicity.ts
@@ -5,7 +5,26 @@ enum Ethnicity {
   HispanicOrLatino = 'HISPANIC_OR_LATINO',
   NativeHawaiianOrOtherPacificIslander = 'NATIVE_HAWAIIAN_OR_OTHER_PACIFIC_ISLANDER',
   White = 'WHITE',
-  Mixed = 'MIXED',
+}
+
+const toStringMappings = {
+  [Ethnicity.AmericanIndianOrAlaskaNative]: 'American Indian or Alaska Native',
+  [Ethnicity.Asian]: 'Asian',
+  [Ethnicity.BlackOrAfricanAmerican]: 'Black or African American',
+  [Ethnicity.HispanicOrLatino]: 'Hispanic or Latino',
+  [Ethnicity.NativeHawaiianOrOtherPacificIslander]:
+    'Native Hawaiian or Other Pacific Islander',
+  [Ethnicity.White]: 'White',
+};
+
+namespace Ethnicity {
+  export function values(): any {
+    return toStringMappings;
+  }
+
+  export function toString(ethnicity: Ethnicity): string {
+    return toStringMappings[ethnicity];
+  }
 }
 
 export default Ethnicity;
