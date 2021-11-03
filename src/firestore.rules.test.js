2f832fa570eb4eed114aa9d36d7a906e4ebfb11c
Josue Rios <josuerios777@gmail.com>
2021-11-02 22:26:18 -0700

    Add firestore rules test

--- src/firestore.rules.test.js
+++ src/firestore.rules.test.js
@@ -19,95 +19,83 @@ const newScholarship = {
     max: 0,
   },
 };
-const authedApp = initializeTestApp({
-  projectId: MY_PROJECT_ID,
-  auth: { uid: 'alice' },
-});
+
 const unauthedApp = initializeTestApp({
   projectId: MY_PROJECT_ID,
 });
-/*
-const authedApp2 = initializeTestApp({
+
+const aliceId = 'alice';
+const aliceApp = initializeTestApp({
+  projectId: MY_PROJECT_ID,
+  auth: { uid: aliceId },
+});
+
+const johnApp = initializeTestApp({
   projectId: MY_PROJECT_ID,
   auth: { uid: 'john-doe' },
 });
 
-const converter = {
-  toFirestore: (data) => {
-    const user = { uid: 'alice', email: 'test' };
-    const author =
-      data.author ?? (user ? { id: user?.uid, email: user?.email } : {});
-
-    console.log({ ...data, author });
-    return { ...data, author };
-  },
-};*/
-
 beforeEach(() => clearFirestoreData({ projectId: MY_PROJECT_ID }));
-afterAll(() => Promise.all([authedApp.delete(), unauthedApp.delete()]));
+afterAll(() =>
+  Promise.all([unauthedApp, aliceApp, johnApp].map((c) => c.delete()))
+);
 
-test('Can read whether you are signed in or not', () => {
-  const testDoc = unauthedApp
-    .firestore()
-    .collection('scholarships')
-    .doc('ASDK91023JUS')
-    .get();
-  return assertSucceeds(testDoc);
+test('allows scholarships read when signed out', () => {
+  return assertSucceeds(
+    unauthedApp.firestore().collection('scholarships').doc('ASDK91023JUS').get()
+  );
 });
 
-test('does not write to scholarships doc when signed out', () => {
-  const testDoc = unauthedApp
-    .firestore()
-    .collection('scholarships')
-    .doc('H12HASJD9')
-    .set(newScholarship);
-  return assertFails(testDoc);
+test('allows scholarships read when signed in', () => {
+  return assertSucceeds(
+    aliceApp.firestore().collection('scholarships').doc('ASDK91023JUS').get()
+  );
 });
 
-test('allows write to scholarships doc when signed in', () => {
-  const testDoc = authedApp
-    .firestore()
-    .collection('scholarships')
-    .doc('KJ019JSD')
-    .set(newScholarship);
-  return assertSucceeds(testDoc);
+test('denies scholarships write when signed out', () => {
+  return assertFails(
+    unauthedApp.firestore().collection('scholarships').doc().set(newScholarship)
+  );
 });
 
-test('allows edit when you are author of scholarship', async () => {
+test('allows scholarships write when signed in', () => {
+  return assertSucceeds(
+    aliceApp.firestore().collection('scholarships').doc().set(newScholarship)
+  );
+});
+
+test('denies scholarships update when user is not author', async () => {
   await assertSucceeds(
-    authedApp
+    aliceApp
       .firestore()
       .collection('scholarships')
       .doc('KLJASDQW')
-      .set(newScholarship)
+      .set({ ...newScholarship, author: { id: aliceId } })
+  );
+
+  return assertFails(
+    johnApp
+      .firestore()
+      .collection('scholarships')
+      .doc('KLJASDQW')
+      .set({ name: 'updated name' })
+  );
+});
+
+test('allows scholarships update when user is author', async () => {
+  await assertSucceeds(
+    aliceApp
+      .firestore()
+      .collection('scholarships')
+      .doc('KLJASDQW')
+      .set({ ...newScholarship, author: { id: aliceId } })
   );
 
   return assertSucceeds(
-    authedApp
+    aliceApp
       .firestore()
       .collection('scholarships')
       .doc('KLJASDQW')
       .set({ ...newScholarship, name: 'update name' })
   );
 });
-
-/* TODO: specify the Firestore connection for our Firestore collections
-test('does not update scholarship when different user', async () => {
-  await assertSucceeds(
-    authedApp
-      .firestore()
-      .collection('scholarships')
-      .withConverter(converter)
-      .doc('KLJASDQW')
-      .set(newScholarship)
-  );
-
-  return assertFails(
-    authedApp2
-      .firestore()
-      .collection('scholarships')
-      .withConverter(converter)
-      .doc('KLJASDQW')
-      .set({ name: 'updated name' })
-  );
-}); */
