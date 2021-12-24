3ec88e0dc67a93b11076f0b54bcae9918f5283c7
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2021-12-24 00:36:41 -0800

    potential fix 2

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -37,7 +37,7 @@ const HomeSection = ({
             alt=""
             style={{
               width: picSize,
-              maxHeight: picSize,
+              minHeight: picSize,
               objectFit: 'contain',
             }}
           />
