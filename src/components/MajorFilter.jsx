3ca028ffa42e2087cc025368c0ffabcaa6e49006
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2022-01-06 21:16:25 -0800

    limit set & drop down arrow

--- src/components/MajorFilter.jsx
+++ src/components/MajorFilter.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
-import { Button, Autocomplete, Popover, Chip, TextField } from '@mui/material';
+import {
+  Button,
+  Autocomplete,
+  Popover,
+  Chip,
+  OutlinedInput,
+  IconButton,
+} from '@mui/material';
 import { MAJORS } from '../types/options';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
 function MajorFilter() {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -20,10 +28,13 @@ function MajorFilter() {
       <Popover
         open={Boolean(anchorEl)}
         anchorEl={anchorEl}
-        onClose={() => setAnchorEl(null)}
+        onClose={() => {
+          setAnchorEl(null);
+          setIsOpen(false);
+        }}
         anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
         transformOrigin={{ vertical: 'top', horizontal: 'left' }}
-        sx={{ maxWidth: 400 }}>
+        PaperProps={{ style: { width: 280 } }}>
         {majors.map((major) => (
           <Chip
             label={major}
@@ -41,18 +52,28 @@ function MajorFilter() {
           freeSolo
           open={isOpen}
           value={majors}
+          disabled={majors.length >= 10}
           onChange={(e, val) => setMajors(val)}
           onInputChange={(e, val) => setIsOpen(val.length > 0)}
           options={[...MAJORS]}
           renderTags={() => null}
           renderInput={(params) => (
-            <TextField
+            <OutlinedInput
               ref={params.InputProps.ref}
               inputProps={params.inputProps}
               autoFocus
-              placeholder="Enter a major to filter by..."
+              placeholder={
+                majors.length < 10
+                  ? 'Enter a major to filter by...'
+                  : 'Limit reached, remove some majors'
+              }
               size="small"
               fullWidth
+              endAdornment={
+                <IconButton onClick={() => setIsOpen(!isOpen)}>
+                  {isOpen ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+                </IconButton>
+              }
             />
           )}
           sx={{ mx: 1, my: 1 }}
