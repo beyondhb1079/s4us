4ba5b8a42e9d2f327cd1dabd9e2846662bfce653
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2022-01-25 18:36:25 -0800

    Refactor filters (#875)
    * Grade filter refactored
    
    * Amount filter refactored
    
    * major filter refactored
    
    * test fail fix

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Grid, FormControl, Select, MenuItem } from '@mui/material';
+import {
+  Grid,
+  FormControl,
+  Select,
+  MenuItem,
+  Button,
+  Popover,
+} from '@mui/material';
 import AmountFilter from './AmountFilter';
 import GradeLevelFilter from './GradeLevelFilter';
 import qParams from '../lib/QueryParams';
 import sortOptions, { DEADLINE_ASC } from '../lib/sortOptions';
 import MajorFilter from './MajorFilter';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+const FilterButton = ({ title, children, popoverWidth }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  return (
+    <>
+      <Button
+        variant="outlined"
+        onClick={(e) => setAnchorEl(e.currentTarget)}
+        endIcon={<ArrowDropDownIcon color="primary" />}
+        sx={{ m: 1, height: (theme) => theme.spacing(4) }}>
+        {title}
+      </Button>
+      <Popover
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+        PaperProps={{ style: popoverWidth }}>
+        {children}
+      </Popover>
+    </>
+  );
+};
+
+FilterButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  popoverWidth: PropTypes.object,
+};
+
+FilterButton.defaultProps = {
+  popoverWidth: undefined,
+};
 
 export default function FilterBar({ setQueryParam, queryParams }) {
   const { minAmount, maxAmount, grades, majors } = queryParams;
@@ -17,28 +60,38 @@ export default function FilterBar({ setQueryParam, queryParams }) {
       justifyContent="space-between"
       sx={{ flexGrow: 1 }}>
       <Grid item>
-        <MajorFilter
-          majors={majors}
-          onSelect={(m) => setQueryParam(qParams.MAJORS, m)}
-          onDelete={(m) =>
-            setQueryParam(
-              qParams.MAJORS,
-              majors.filter((major) => major !== m)
-            )
-          }
-        />
+        <FilterButton title="Majors" popoverWidth={{ minWidth: 280 }}>
+          <MajorFilter
+            majors={majors}
+            onSelect={(m) => setQueryParam(qParams.MAJORS, m)}
+            onDelete={(m) =>
+              setQueryParam(
+                qParams.MAJORS,
+                majors.filter((major) => major !== m)
+              )
+            }
+          />
+        </FilterButton>
 
-        <GradeLevelFilter
-          grades={new Set(grades)}
-          changeFn={(e) => setQueryParam(qParams.GRADES, e)}
-        />
+        <FilterButton title="Grades">
+          <GradeLevelFilter
+            grades={new Set(grades)}
+            changeFn={(e) => setQueryParam(qParams.GRADES, e)}
+          />
+        </FilterButton>
 
-        <AmountFilter
-          min={minAmount ?? 0}
-          max={maxAmount ?? 0}
-          onMinChange={(e) => setQueryParam(qParams.MIN_AMOUNT, e.target.value)}
-          onMaxChange={(e) => setQueryParam(qParams.MAX_AMOUNT, e.target.value)}
-        />
+        <FilterButton title="Amount">
+          <AmountFilter
+            min={minAmount ?? 0}
+            max={maxAmount ?? 0}
+            onMinChange={(e) =>
+              setQueryParam(qParams.MIN_AMOUNT, e.target.value)
+            }
+            onMaxChange={(e) =>
+              setQueryParam(qParams.MAX_AMOUNT, e.target.value)
+            }
+          />
+        </FilterButton>
       </Grid>
 
       <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
