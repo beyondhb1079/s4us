740e5d8ac83107edde30a8e9d59a895e621a524b
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2022-01-31 20:47:57 -0800

    sort button styling

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,54 +1,62 @@
-import React from 'react';
-import {
-  Grid,
-  FormControl,
-  Select,
-  MenuItem,
-  IconButton,
-  Typography,
-} from '@mui/material';
+import React, { useState } from 'react';
+import { Grid, Menu, MenuItem, IconButton, Typography } from '@mui/material';
 import useQueryParams from '../lib/useQueryParams';
 import sortOptions, { DEADLINE_ASC } from '../lib/sortOptions';
 import TuneIcon from '@mui/icons-material/Tune';
 import PropTypes from 'prop-types';
+import ImportExportIcon from '@mui/icons-material/ImportExport';
 
 export default function FilterBar({ openFilter }) {
   const [{ sortBy }, setQueryParam] = useQueryParams();
+  const [anchorEl, setAnchorEl] = useState(null);
 
   return (
-    <Grid
-      container
-      justifyContent="space-between"
-      alignItems="center"
-      direction="row-reverse"
-      sx={{ flexGrow: 1, bgcolor: 'primary.main' }}>
-      <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
-        Sort by
-        <FormControl variant="outlined" sx={{ margin: 1, minWidth: 120 }}>
-          <Select
-            value={sortBy ?? DEADLINE_ASC}
-            onChange={(e) => setQueryParam('sortBy', e.target.value)}
-            displayEmpty
-            sx={{ height: (theme) => theme.spacing(4) }}>
-            {Object.keys(sortOptions).map((key) => (
-              <MenuItem key={key} value={key}>
-                {sortOptions[key]}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Grid>
-
+    <>
       <Grid
-        item
-        sx={{ display: { xs: 'flex', md: 'none' }, alignItems: 'center' }}>
-        <IconButton onClick={openFilter} color="secondary">
-          <TuneIcon />
-        </IconButton>
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        direction="row-reverse"
+        sx={{ flexGrow: 1, bgcolor: 'primary.main', p: 2 }}>
+        <Grid item sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton
+            onClick={(e) => setAnchorEl(e.currentTarget)}
+            color="secondary">
+            <ImportExportIcon />
+          </IconButton>
+          <Typography color="secondary">Sort</Typography>
+        </Grid>
 
-        <Typography color="secondary">Filter</Typography>
+        <Grid
+          item
+          sx={{ display: { xs: 'flex', md: 'none' }, alignItems: 'center' }}>
+          <IconButton onClick={openFilter} color="secondary">
+            <TuneIcon />
+          </IconButton>
+
+          <Typography color="secondary">Filter</Typography>
+        </Grid>
       </Grid>
-    </Grid>
+
+      <Menu
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}>
+        {Object.keys(sortOptions).map((key) => (
+          <MenuItem
+            key={key}
+            selected={key === (sortBy ?? DEADLINE_ASC)}
+            onClick={() => {
+              setQueryParam('sortBy', key);
+              setAnchorEl(null);
+            }}>
+            {sortOptions[key]}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
   );
 }
 
