11a2373d38e8221379bcf7687825edcf1cebea83
Josue Rios <josuerios777@gmail.com>
2022-02-03 23:39:42 -0800

    Hide app bar and slide in filter bar on scroll (#908)

--- src/pages/ListScholarships.jsx
+++ src/pages/ListScholarships.jsx
@@ -1,16 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
-import { Box, Container, Drawer, useMediaQuery } from '@mui/material';
+import {
+  Box,
+  Collapse,
+  Container,
+  Drawer,
+  Toolbar,
+  useMediaQuery,
+  useScrollTrigger,
+} from '@mui/material';
 import FilterBar from '../components/FilterBar';
 import FilterPanel from '../components/FilterPanel';
 import ScholarshipList from '../components/ScholarshipList';
 import useQueryParams from '../lib/useQueryParams';
 import { DEADLINE_ASC, getDir, getField } from '../lib/sortOptions';
-import { useNavigationType } from 'react-router-dom';
+import { HeaderSkeleton } from '../components/Header';
 
-const drawerWidth = { xs: '100%', md: 360 };
-const SCROLL_KEY = 'scholarship-list-scroll-top';
+const drawerWidth = 360;
 
 function ListScholarships() {
   const { t } = useTranslation();
@@ -33,55 +40,47 @@ function ListScholarships() {
     hideExpired: true,
   };
 
-  const ref = useRef('main');
-  const navigationType = useNavigationType();
-
-  // This runs when the main component is first rendered.
-  useEffect(() => {
-    // Restore the saved scroll position if we went backwards/forwards in history.
-    if (navigationType === 'POP') {
-      ref.current.scrollTop = sessionStorage.getItem(SCROLL_KEY);
-    }
-
-    // Listen for scroll events, saving scroll position for later.
-    ref.current.addEventListener(
-      'scroll',
-      () => sessionStorage.setItem(SCROLL_KEY, ref.current.scrollTop),
-      { passive: true }
-    );
-  }, [ref, navigationType]);
+  const scrollTrigger = useScrollTrigger();
 
   return (
-    <Box sx={{ display: 'flex', height: '100vh' }}>
+    <Box sx={{ display: 'flex' }}>
       <Helmet>
         <title>{t('listScholarships.titleTag')}</title>
       </Helmet>
       <Drawer
         sx={{
           flexShrink: 0,
-          width: drawerWidth,
+          width: { xs: '100%', md: drawerWidth },
           '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            position: 'absolute',
+            width: { xs: '100%', md: drawerWidth },
             boxSizing: 'border-box',
           },
-          position: { xs: 'static', md: 'sticky' },
-          overflowY: 'auto',
         }}
         open={drawerOpen || isDesktop}
         variant={isDesktop ? 'permanent' : 'temporary'}
         anchor="left">
-        <FilterPanel onClose={() => setDrawerOpen(false)} />
+        <Collapse in={!scrollTrigger} sx={{ flexShrink: 0 }}>
+          <HeaderSkeleton />
+        </Collapse>
+        <Box sx={{ overflowY: 'auto', flexGrow: 1 }}>
+          <FilterPanel onClose={() => setDrawerOpen(false)} />
+        </Box>
       </Drawer>
 
-      <Box
-        component="main"
-        ref={ref}
-        sx={{
-          width: '100%',
-          overflowY: 'auto',
-        }}>
-        <FilterBar openFilter={() => setDrawerOpen(true)} />
+      <Box component="main" sx={{ width: '100%' }}>
+        <Box
+          sx={{
+            position: 'fixed',
+            top: 0,
+            width: { xs: '100%', md: `calc(100% - ${drawerWidth}px)` },
+            zIndex: 1,
+          }}>
+          <Collapse in={!scrollTrigger} sx={{ flexShrink: 0 }}>
+            <HeaderSkeleton />
+          </Collapse>
+          <FilterBar openFilter={() => setDrawerOpen(true)} />
+        </Box>
+        <Toolbar />
 
         <Container maxWidth="md" sx={{ flexGrow: 1 }}>
           <ScholarshipList filters={queryFilters} />
