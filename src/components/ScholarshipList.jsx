35ecca0f8d7344f6c9f7d5377650da27678d718e
Josue Rios <josuerios777@gmail.com>
2022-02-21 23:48:00 -0800

    Auto load more scholarships

--- src/components/ScholarshipList.jsx
+++ src/components/ScholarshipList.jsx
@@ -1,15 +1,10 @@
-import React, { useEffect, useContext } from 'react';
-import {
-  Box,
-  Button,
-  CircularProgress,
-  Stack,
-  Typography,
-} from '@mui/material';
+import React, { useContext, useEffect, useRef } from 'react';
+import { Box, CircularProgress, Stack, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import ScholarshipCard from './ScholarshipCard';
 import ScholarshipsContext from '../models/ScholarshipsContext';
+import useOnScreen from '../lib/useOnScreen';
 
 export default function ScholarshipList({ noResultsNode, filters }) {
   const { canLoadMore, error, loading, loadMore, scholarships, setFilters } =
@@ -19,39 +14,42 @@ export default function ScholarshipList({ noResultsNode, filters }) {
   // Resets result context if filters change.
   useEffect(() => setFilters(filters), [filters, setFilters]);
 
+  // Automatically load more when the progress is visible
+  const progressRef = useRef(null);
+  const progressVisible = useOnScreen(progressRef);
+
+  useEffect(() => {
+    if (progressVisible && canLoadMore && !loading) {
+      loadMore();
+    }
+  }, [loading, canLoadMore, loadMore, progressVisible]);
+
   return (
-    <Stack spacing={3}>
+    <Stack spacing={3} paddingY={3}>
       {scholarships.map(({ id, data }) => (
         <ScholarshipCard key={id} scholarship={{ id, data }} style="result" />
       ))}
       <Box sx={{ margin: 'auto', textAlign: 'center' }}>
-        {(() => {
-          if (error) return <Typography>{error.toString()}</Typography>;
-          if (loading)
-            return (
-              <CircularProgress
-                data-testid="progress"
-                sx={{ display: 'block', margin: 'auto' }}
-              />
-            );
-          if (canLoadMore)
-            return (
-              <Button color="primary" onClick={loadMore}>
-                {t('btn.loadMore')}
-              </Button>
-            );
-          if (scholarships?.length)
-            return (
-              <Typography>{t('listScholarships.endOfResults')}</Typography>
-            );
-          return (
+        {error && <Typography>{error.toString()}</Typography>}
+        <CircularProgress
+          data-testid="progress"
+          ref={progressRef}
+          sx={{
+            display: loading || canLoadMore ? 'block' : 'none',
+            margin: 'auto',
+          }}
+        />
+
+        {!canLoadMore &&
+          (scholarships?.length ? (
+            <Typography>{t('listScholarships.endOfResults')}</Typography>
+          ) : (
             noResultsNode || (
               <Typography>
                 {t('listScholarships.noScholarshipsFound')}
               </Typography>
             )
-          );
-        })()}
+          ))}
       </Box>
     </Stack>
   );
