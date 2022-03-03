b957c4d285a6af66cfc529a57d4b0ba4197bf26d
Job <lambdainsphere@Jobs-MacBook-Air.local>
2022-03-03 13:06:59 -0800

    add tests for HeaderNavMenu component

---
+++ src/components/HeaderNavMenu.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import HeaderNavMenu from './HeaderNavMenu';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const links = {
+  topology: '/topology',
+  'abstract-algebra': '/abstract-algebra',
+  'differential-geometry': '/differential-geometry',
+};
+
+describe('HeaderNavMenu', () => {
+  test('renders App component', () => {
+    render(
+      <Router>
+        <HeaderNavMenu links={links} />
+      </Router>
+    );
+  });
+  test('renders Header links', () => {
+    render(
+      <Router>
+        <HeaderNavMenu links={links} />
+      </Router>
+    );
+    expect(document.querySelector('a').getAttribute('href')).toBe('/topology');
+    expect(screen.getByText('abstract-algebra').closest('a')).toHaveAttribute(
+      'href',
+      '/abstract-algebra'
+    );
+
+    expect(
+      screen.getByText('differential-geometry').closest('a')
+    ).toHaveAttribute('href', '/differential-geometry');
+  });
+});
