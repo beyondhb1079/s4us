15f872a339c26e4366b0df4fc424c80fc4c4c5da
Gonzalo Jr <64123425+gonzalo1330@users.noreply.github.com>
2022-03-24 17:38:00 -0700

    initial styling changes to the login dialog to match figma design (#880)

--- src/components/LoginDialog.jsx
+++ src/components/LoginDialog.jsx
@@ -5,9 +5,14 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Grid,
+  IconButton,
+  Typography,
+  Box,
 } from '@mui/material';
 import firebase from 'firebase';
 import StyleFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 export default function LoginDialog() {
   const location = useLocation();
@@ -38,16 +43,63 @@ export default function LoginDialog() {
       open={showLoginDialog}
       onClose={closeDialog}
       aria-labelledby="responsive-dialog-title">
-      <DialogTitle id="responsive-dialog-title">
-        Login using your account or email.
-      </DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          You can login using your existing account to authenticate and log in
-          to our web-app. You can create an account if you do not want to use
-          your existing account to interact with our app.
-        </DialogContentText>
-        <StyleFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      <DialogContent sx={{ p: 0 }}>
+        <Box sx={{ position: 'absolute', top: 0, right: 0 }}>
+          <IconButton
+            size="medium"
+            aria-haspopup="true"
+            onClick={() => closeDialog()}
+            color="inherit">
+            <CancelIcon
+              sx={{ color: { xs: 'background.paper', sm: 'inherit' } }}
+            />
+          </IconButton>
+        </Box>
+        <Grid container spacing={2} alignItems="center">
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ color: 'background.paper', bgcolor: 'primary.main' }}>
+            <DialogTitle id="responsive-dialog-brand">
+              <Typography sx={{ color: 'background.paper' }}>
+                DreamScholars
+              </Typography>
+            </DialogTitle>
+
+            <DialogTitle id="responsive-dialog-welcome">
+              <Typography variant="h4" sx={{ color: 'background.paper' }}>
+                Welcome.
+              </Typography>
+            </DialogTitle>
+
+            <DialogContentText sx={{ color: 'background.paper', p: 3 }}>
+              <Typography paragraph sx={{ color: 'background.paper' }}>
+                DreamScholars provides scholarships for all students regardless
+                of status.
+              </Typography>
+              <Typography paragraph sx={{ color: 'background.paper' }}>
+                Join our community &amp;
+              </Typography>
+              <Typography paragraph sx={{ color: 'background.paper' }}>
+                Get access to scholarship submissions and other cool features
+                soon.
+              </Typography>
+            </DialogContentText>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <DialogTitle
+              id="responsive-dialog-title"
+              sx={{ textAlign: 'center' }}>
+              <Typography sx={{ fontWeight: 'bold' }}>Sign In</Typography>
+            </DialogTitle>
+            <StyleFirebaseAuth
+              uiConfig={uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          </Grid>
+        </Grid>
       </DialogContent>
     </Dialog>
   );
