64b35c81417de50e44e51b24d45699df1494882b
Job <lambdainsphere@Jobs-MacBook-Air.local>
2022-04-02 21:36:47 -0700

    finish up.

--- src/components/ShowMoreScholarshipCard.jsx
+++ src/components/ShowMoreScholarshipCard.jsx
@@ -1,35 +1,36 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import React from 'react';
 
-export default function ShowMoreScholarshipCard(scholarship) {
-  const {
-    name,
-    organization,
-    amount,
-    deadline,
-    website,
-    description,
-    tags,
-    reqs,
-    author,
-  } = scholarship.data;
-  console.log(scholarship.data);
-  // dummy variables
-  const org = organization;
-  console.log(org);
-  const web = website;
-  console.log(web);
-  console.log(tags);
-  console.log(reqs);
-  console.log(author);
+export default function ShowMoreScholarshipCard({ scholarship }) {
+  const getAmount = (amountData) => {
+    if (amountData.min === amountData.max) {
+      return amountData.min;
+    } else {
+      return (
+        '$' + amountData.min.toString() + '-' + '$' + amountData.max.toString()
+      );
+    }
+  };
+
+  const scholarshipName = scholarship.data.name;
+  const amount = scholarship.data.amount;
+  const scholarshipAmount = getAmount(amount);
+  const date = new Date(scholarship.data.deadline);
+  const dateDeadline =
+    date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear();
+
+  const description = scholarship.data.description;
 
   return (
     <Card>
       <CardContent>
-        <Typography>{name}</Typography>
-        <Typography>{amount}</Typography>
-        <Typography>{deadline}</Typography>
+        <Typography>
+          <b>{scholarshipName}</b>
+        </Typography>
+        <Typography>{scholarshipAmount}</Typography>
+        <Typography>Deadline: {dateDeadline}</Typography>
         <Typography>{description}</Typography>
       </CardContent>
     </Card>
