a6c7a75e2e335be08c2390558ed1c25ca20871ba
Job Henandez Lara <hj93@protonmail.com>
2022-04-28 19:43:21 -0700

    Show more scholarships (#965)
    * add show more scholarship component.
    
    * build show more scholarships components.
    
    * fix bug
    
    * fix bug
    
    * remove some tests
    
    * comment out some unused code for draft purposes
    
    * comment out some unused imports for drafting purposes.
    
    * check for typos.
    
    * finish up.
    
    * last touches.
    
    * address review.
    
    * address review.
    
    * finish addressing review.
    
    * remove not necessary function.
    
    Co-authored-by: Job <lambdainsphere@Jobs-MacBook-Air.local>

---
+++ src/components/ShowMoreScholarships.jsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import ScholarshipsContext from '../models/ScholarshipsContext';
+import Grid from '@mui/material/Grid';
+import ScholarshipCard from './ScholarshipCard';
+
+export default function ShowMoreScholarships({ currentId }) {
+  const { scholarships } = useContext(ScholarshipsContext);
+
+  return (
+    <Grid container spacing={2} justify="center">
+      {scholarships
+        .filter((s) => s.id !== currentId)
+        .slice(0, 3)
+        .map(({ id, data }) => {
+          return (
+            <Grid key={id} item xs={12} sm={6} md={4}>
+              <ScholarshipCard scholarship={{ id, data }} />
+            </Grid>
+          );
+        })}
+    </Grid>
+  );
+}
