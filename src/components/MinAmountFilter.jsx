29210a982c0b59a155106b1d2dc094ff1e33628c
edwingl10 <40483569+edwingl10@users.noreply.github.com>
2022-05-11 22:16:27 -0700

    style changes

--- src/components/MinAmountFilter.jsx
+++ src/components/MinAmountFilter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { InputLabel, Stack, Slider } from '@mui/material';
+import { Stack, Slider } from '@mui/material';
 import AmountTextField from './AmountTextField';
 
 export default function MinAmountFilter(props) {
@@ -8,17 +8,11 @@ export default function MinAmountFilter(props) {
   const error = min < 0;
 
   return (
-    <Stack alignItems="center" sx={{ maxWidth: 300, m: 'auto' }}>
-      <>
-        <InputLabel>Min</InputLabel>
-        <AmountTextField
-          error={error}
-          value={min || ''}
-          onChange={(e) => onMinChange(e.target.value)}
-          placeholder="No min"
-        />
-      </>
-
+    <Stack
+      alignItems="center"
+      direction="row"
+      spacing={2}
+      sx={{ maxWidth: 300, m: 'auto' }}>
       <Slider
         value={min}
         min={0}
@@ -26,6 +20,14 @@ export default function MinAmountFilter(props) {
         step={100}
         onChange={(e) => onMinChange(e.target.value)}
         sx={{ mt: 1 }}
+      />
+
+      <AmountTextField
+        error={error}
+        value={min || ''}
+        onChange={(e) => onMinChange(e.target.value)}
+        variant="standard"
+        placeholder="No min"
       />
     </Stack>
   );
