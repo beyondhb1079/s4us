c400c1031e009a8a0dce59b183c963921d2abc63
Josue Rios <josuerios777@gmail.com>
2022-05-12 17:26:11 -0700

    Speed up Landing home page performance (#1103)

--- src/lib/useAuth.tsx
+++ src/lib/useAuth.tsx
@@ -6,13 +6,22 @@ import {
   ReactNode,
   useContext,
 } from 'react';
-import firebase from 'firebase';
+
+// Copied from 'firebase' to lazily load it.
+interface UserInfo {
+  displayName: string | null;
+  email: string | null;
+  phoneNumber: string | null;
+  photoURL: string | null;
+  providerId: string;
+  uid: string;
+}
 
 interface Auth {
   /** Claims for the current user, if any. */
   claims?: Record<string, any>;
   /** The currently logged in user. May be `undefined` initially. */
-  currentUser?: firebase.User | null;
+  currentUser?: UserInfo | null;
 }
 
 const authContext = createContext({} as Auth);
@@ -25,15 +34,16 @@ export default function useAuth(): Auth {
 // Provider hook that creates auth object and handles state
 function useProvideAuth(): Auth {
   const [currentUser, setCurrentUser] = useState(
-    firebase.auth().currentUser || (undefined as undefined | null)
+    undefined as undefined | null | UserInfo
   );
   const [claims, setClaims] = useState({} as Record<string, any>);
 
   // Subscribe to user on mount. We need to have this called only once so only
   // AuthProvider should use it
-  useEffect(
-    () =>
-      firebase.auth().onAuthStateChanged((user) => {
+  useEffect(() => {
+    let cleanup = () => {};
+    import('firebase').then((firebase) => {
+      cleanup = firebase.default.auth().onAuthStateChanged((user) => {
         setCurrentUser(user);
         if (user) {
           user
@@ -43,9 +53,10 @@ function useProvideAuth(): Auth {
         } else {
           setClaims({});
         }
-      }),
-    []
-  );
+      });
+    });
+    return () => cleanup();
+  }, []);
 
   return { currentUser, claims };
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import PublicHome from './PublicHome';
-import UserHome from './UserHome';
 import useAuth from '../lib/useAuth';
+
+const UserHome = lazy(() => import('./UserHome'));
 
 export default function Home(): JSX.Element | string {
   const { currentUser: user } = useAuth();
