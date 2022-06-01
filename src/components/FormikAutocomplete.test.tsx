ada062765db6ac4f5c5f63f07f15487567b25cc3
Josue Rios <josuerios777@gmail.com>
2022-05-31 22:38:18 -0700

    fix lint

--- src/components/FormikAutocomplete.test.tsx
+++ src/components/FormikAutocomplete.test.tsx
@@ -2,7 +2,6 @@ import { render } from '@testing-library/react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import FormikAutocomplete from './FormikAutocomplete';
 import userEvent from '@testing-library/user-event';
-import { sl } from 'date-fns/locale';
 
 const renderWithTheme = (ui: JSX.Element) =>
   render(<ThemeProvider theme={createTheme()}>{ui}</ThemeProvider>);
