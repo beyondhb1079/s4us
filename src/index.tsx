ba3015d28ac25b74bcabdb29381b8ab4be9bf594
Josue Rios <josuerios777@gmail.com>
2026-02-21 22:56:59 -0800

    fix(react): update ReactDOM.render to createRoot for React 18+

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <Suspense fallback="">
       <I18nextProvider i18n={i18n}>
@@ -14,7 +17,6 @@ ReactDOM.render(
       </I18nextProvider>
     </Suspense>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
